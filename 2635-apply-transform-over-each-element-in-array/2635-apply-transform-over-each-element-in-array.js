e5230d7aafd329897217aac12b47a1b293c364e3
Mahesh Avvaru <maheshavvaru2004@gmail.com>
2024-01-26 14:15:08 +0530

    Time: 44 ms (91.93%), Space: 48.6 MB (7.09%) - LeetHub

--- 2635-apply-transform-over-each-element-in-array/2635-apply-transform-over-each-element-in-array.js
+++ 2635-apply-transform-over-each-element-in-array/2635-apply-transform-over-each-element-in-array.js
@@ -4,11 +4,5 @@
  * @return {number[]}
  */
 var map = function(arr, fn) {
-    var result = [];
-    
-    for (var i = 0; i < arr.length; i++) {
-        result.push(fn(arr[i], i));
-    }
-    
-    return result;
+    return arr.map((ele, index) => fn(ele, index))
 };
