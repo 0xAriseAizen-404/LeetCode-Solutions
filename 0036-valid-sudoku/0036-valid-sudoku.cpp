225a6363fa2e209c19f98c2d9ce177836f8c0e0e
Mahesh Avvaru <maheshavvaru2004@gmail.com>
2024-02-07 19:20:48 +0530

    Time: 29 ms (13.46%), Space: 25 MB (5.08%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.cpp
+++ 0036-valid-sudoku/0036-valid-sudoku.cpp
@@ -1,20 +1,22 @@
 class Solution {
-    public boolean isValidSudoku(char[][] board) {
-        HashMap<String, Integer> hMap = new HashMap<>();
-        for(int i=0;i<9;++i) {
-            for(int j=0;j<9;++j) {
+public:
+    bool isValidSudoku(vector<vector<char>>& board) {
+        unordered_map<string, int> map;
+        for(int i=0; i<9; ++i) {
+            for(int j=0; j<9; ++j) {
                 char currVal = board[i][j];
                 if(currVal != '.') {
-                    if((hMap.get(currVal+"row"+i)!=null) || (hMap.get(currVal+"col"+j)!=null) || (hMap.get(currVal+"grid"+(i/3)+"-"+(j/3))!=null))
-                        return false; // Already Exists
-                    else {
-                        hMap.put((currVal+"row"+i),1);
-                        hMap.put((currVal+"col"+j),1);
-                        hMap.put((currVal+"grid"+(i/3)+"-"+(j/3)),1);
+                    string row_key = to_string(i) + "row" + currVal;
+                    string col_key = to_string(j) + "col" + currVal;
+                    string grid_key = to_string(i/3) + "-" + to_string(j/3) + "grid" + currVal;
+                    if(map[row_key] != 0 || map[col_key] != 0 || map[grid_key] != 0) return false;
+                    else 
+                        map[row_key]++;
+                        map[col_key]++;
+                        map[grid_key]++;
                     }
                 }
             }
-        }
         return true;
     }
-}
+};
