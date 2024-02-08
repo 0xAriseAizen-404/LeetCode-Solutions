482450dd72357016fba3f045a0ff389f9855876b
Mahesh Avvaru <maheshavvaru2004@gmail.com>
2024-02-08 23:35:42 +0530

    Time: 8 ms (37.58%), Space: 8.5 MB (30.08%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.cpp
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -1,42 +1,27 @@
 class Solution {
-    public boolean isPalindrome(String s) {
-        if (s.isEmpty()) {
-        	return true;
+public:
+    bool isPalindrome(string s) {
+        int left = 0, right = s.length() - 1;
+        
+        while (left < right) {
+            // Skip non-alphanumeric characters
+            while (left < right && !isalnum(s[left])) {
+                left++;
+            }
+            while (left < right && !isalnum(s[right])) {
+                right--;
+            }
+            
+            // Compare characters (after converting to lowercase)
+            if (tolower(s[left]) != tolower(s[right])) {
+                return false;
+            }
+            
+            // Move pointers towards the center
+            left++;
+            right--;
         }
-        int start = 0;
-        int last = s.length() - 1;
-        while(start < last) {
-        	char currFirst = s.charAt(start);
-        	char currLast = s.charAt(last);
-        	if (!Character.isLetterOrDigit(currFirst )) {
-        		start++;
-        	} else if(!Character.isLetterOrDigit(currLast)) {
-        		last--;
-        	} else {
-        		if (Character.toLowerCase(currFirst) != Character.toLowerCase(currLast)) {
-        			return false;
-        		}
-        		start++;
-        		last--;
-        	}
-        }
+        
         return true;
-        
-//         s = s.toLowerCase();
-//         StringBuilder b = new StringBuilder(); 
-        
-//         for(int x =0; x <s.length(); x++){
-//             if(Character.isLetterOrDigit(s.charAt(x))){
-//                 b.append(s.charAt(x));
-//             }
-//         }
-//         return recursively(b,0,b.length());        
-//     }    
-//     public boolean recursively(StringBuilder b,int i,int n){      
-//       if(i>= n/2) return true;          
-//           if(b.charAt(i) != b.charAt(n-i-1)){
-//               return false;
-//           }
-//           return recursively(b,i+1,n);
     }
-}
+};
