0a0eecc189a250605df4d25faf699ecce6237ff4
Mahesh Avvaru <maheshavvaru2004@gmail.com>
2024-02-09 22:57:33 +0530

    Time: 0 ms (100.00%), Space: 42 MB (67.74%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.java
+++ 0048-rotate-image/0048-rotate-image.java
@@ -1,17 +1,37 @@
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
 class Solution {
-public:
-    void rotate(vector<vector<int>>& mat) {
-        int n = mat.size();
-        //Transpose
-        for(int i=0;i<n-1;++i){
-            for(int j=i+1;j<n;++j){
-                mat[i][j] = mat[i][j]^mat[j][i];
-                mat[j][i] = mat[i][j]^mat[j][i];
-                mat[i][j] = mat[i][j]^mat[j][i];
-                //swap(mat[i][j],mat[j][i]);//pre - algorithm
+    public void rotate(int[][] mat) {
+        int n = mat.length;
+
+        // Transpose
+        for (int i = 0; i < n - 1; ++i) {
+            for (int j = i + 1; j < n; ++j) {
+                mat[i][j] = mat[i][j] ^ mat[j][i];
+                mat[j][i] = mat[i][j] ^ mat[j][i];
+                mat[i][j] = mat[i][j] ^ mat[j][i];
             }
         }
-        for(int i=0;i<n;++i)
-            reverse(mat[i].begin(),mat[i].end());
+        
+        for (int i = 0; i < n; ++i) {
+            int start = 0;
+            int end = n - 1;
+            while (start < end) {
+                int temp = mat[i][start];
+                mat[i][start] = mat[i][end];
+                mat[i][end] = temp;
+                start++;
+                end--;
+            }
+        }
+
+        // Reverse each row
+        // for (int i = 0; i < n; ++i) {
+            // List<Integer> rowList = Arrays.asList(Arrays.stream(mat[i]).boxed().toArray(Integer[]::new));
+            // Collections.reverse(Arrays.asList(mat[i]));
+            // mat[i] = rowList.stream().mapToInt(Integer::intValue).toArray();
+        // }
     }
-};
+}
