994c531c1f58dca04a15ba5985a596ade10def46
Mahesh Avvaru <maheshavvaru2004@gmail.com>
2024-02-10 22:46:37 +0530

    Time: 33 ms (53.34%), Space: 51.3 MB (64.23%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,35 +1,3 @@
-// class Solution {
-//     public List<List<Integer>> threeSum(int[] nums) {
-//         List<List<Integer>> ans = new ArrayList<>();
-//         Arrays.sort(nums);
-//         for (int i = 0; i < nums.length; ++i) {
-//             int low = i + 1;
-//             int high = nums.length - 1;
-//             while (low < high) {
-//                 int sum = nums[i] + nums[low] + nums[high];
-//                 if (sum == 0) {
-//                     ans.add(Arrays.asList(nums[i], nums[low], nums[high]));
-
-//                     low++;
-//                     high--;
-
-//                     while (low < high && nums[low - 1] == nums[low]) low++;
-//                     while (low < high && nums[high + 1] == nums[high]) high--;
-//                 } else if (sum > 0) {
-//                     high--;
-//                 } else {
-//                     low++;
-//                 }
-//             }
-//             while ((i + 1) < nums.length && nums[i] == nums[i + 1]) i++;
-//         }
-
-//         Set<List<Integer>> uniqueTriplets = new HashSet<>(ans);
-//         return new ArrayList<>(uniqueTriplets);
-//     }
-// }
-
-
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
         List<List<Integer>> ans = new ArrayList<>();
