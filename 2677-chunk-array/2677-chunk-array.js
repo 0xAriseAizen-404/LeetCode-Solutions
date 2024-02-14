c5b043e710bf05f06045ea92d670692835b79ef7
Mahesh Avvaru <maheshavvaru2004@gmail.com>
2024-02-14 14:04:30 +0530

    Time: 53 ms (73.87%), Space: 51.3 MB (29.62%) - LeetHub

---
+++ 2677-chunk-array/2677-chunk-array.js
@@ -0,0 +1,11 @@
+/**
+ * @param {Array} arr
+ * @param {number} size
+ * @return {Array}
+ */
+var chunk = function(arr, size) {
+    const chunkedArray = []
+    for(let index=0; index<arr.length; index += size)
+        chunkedArray.push(arr.slice(index, index + size))
+    return chunkedArray
+};
