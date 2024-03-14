f9683b1f2a026ada7a093147aab1f4dd7c1179c5
Mahesh Avvaru <maheshavvaru2004@gmail.com>
2024-03-14 20:04:42 +0530

    Time: 0 ms (100.00%), Space: 44.5 MB (37.55%) - LeetHub

--- 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.java
+++ 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.java
@@ -1,29 +1,28 @@
 /**
  * Definition for singly-linked list.
- * struct ListNode {
+ * class ListNode {
  *     int val;
- *     ListNode *next;
- *     ListNode(int x) : val(x), next(NULL) {}
- * };
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
  */
-class Solution {
-public:
-    ListNode *detectCycle(ListNode *head) {
-        ListNode* slow = head;
-        ListNode* fast = head;
-        while(fast!=NULL && fast->next!=NULL) {
-            slow = slow->next;
-            fast = fast->next->next;
-            if(slow == fast) 
-                break;
+public class Solution {
+    public ListNode detectCycle(ListNode head) {
+        ListNode slow = head, fast = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+            if (slow == fast) break;
         }
-        if(fast==NULL || fast->next==NULL)
-            return NULL;
+        if (fast == null || fast.next == null) return null;
         fast = head;
-        while(fast != slow) {
-            slow = slow->next;
-            fast = fast->next;
+        while (fast != slow) {
+            fast = fast.next;
+            slow = slow.next;
         }
-        return slow;
+        return fast;
     }
-};
+}
