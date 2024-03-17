f2a79f8180829b4d1b37a547b83cf0ce3ae2cf3f
Mahesh Avvaru <maheshavvaru2004@gmail.com>
2024-03-17 12:04:10 +0530

    Time: 23 ms (70.14%), Space: 21.1 MB (86.49%) - LeetHub

--- 0143-reorder-list/0143-reorder-list.cpp
+++ 0143-reorder-list/0143-reorder-list.cpp
@@ -11,26 +11,63 @@
 class Solution {
 public:
     void reorderList(ListNode* head) {
-        vector<int>v;
-        ListNode *temp=head;
-        while(temp!=NULL){
-            int y=temp->val;
-            v.push_back(y);
-            temp=temp->next;
+        if (!head || !head->next || !head->next->next) {
+            return;
         }
-         int n = v.size();
-        int i=0, j=n-1;
-        temp = head;
-        while (i < j) {
-            temp->val = v[i];
-            temp=temp->next;
-            i++;
-            temp->val=v[j];
-            temp=temp->next;
-            j--;
+
+        // Step 1: Find the middle of the list
+        ListNode *slow = head;
+        ListNode *fast = head;
+
+        while (fast->next && fast->next->next) {
+            slow = slow->next;
+            fast = fast->next->next;
         }
-        if (i==j) {
-            temp->val=v[i];
+
+        // Step 2: Reverse the second half of the list
+        ListNode *prev = nullptr;
+        ListNode *curr = slow->next;
+        slow->next = nullptr;
+
+        while (curr) {
+            ListNode *nextNode = curr->next;
+            curr->next = prev;
+            prev = curr;
+            curr = nextNode;
         }
+
+        // Step 3: Merge the first half and the reversed second half
+        ListNode *first = head;
+        ListNode *second = prev;
+
+        while (second) {
+            ListNode *nextFirst = first->next;
+            ListNode *nextSecond = second->next;
+            first->next = second;
+            second->next = nextFirst;
+            first = nextFirst;
+            second = nextSecond;
+        }
+        // vector<int>v;
+        // ListNode *temp=head;
+        // while(temp!=NULL){
+        //     int y=temp->val;
+        //     v.push_back(y);
+        //     temp=temp->next;
+        // }
+        //  int n = v.size();
+        // int i=0, j=n-1;
+        // temp = head;
+        // while (i < j) {
+        //     temp->val = v[i];
+        //     temp=temp->next;
+        //     i++;
+        //     temp->val=v[j];
+        //     temp=temp->next;
+        //     j--;
+        // }
+        // if (i==j) {
+        //     temp->val=v[i];
+        // }
     }
 };
