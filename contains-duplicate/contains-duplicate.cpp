91c091ac458610f9e1ca91be3f2a0d82d0063d3a
Mahesh Avvaru <maheshavvaru2004@gmail.com>
2024-03-17 22:17:17 +0530

    Time: 85 ms, Memory: 73.2 MB - LeetHub

--- contains-duplicate/contains-duplicate.cpp
+++ contains-duplicate/contains-duplicate.cpp
@@ -1,10 +1,11 @@
 class Solution {
-    public boolean containsDuplicate(int[] nums) {
-        HashMap<Integer, Boolean> hmap = new HashMap<>();
-        for(int x : nums) {
-            if(hmap.containsKey(x)) return true;
-            else hmap.put(x, true);
-        }
-        return false;
-    }
-}
+public:
+     bool containsDuplicate(vector<int>& nums) {
+         unordered_map<int,int> umap;
+         for(auto x : nums){
+             if(umap[x]) return true;
+             umap[x]++;
+         }
+         return false;
+     }  
+};
