47630677044b799cfa92f6af74b46b2c7959cb93
Mahesh Avvaru <maheshavvaru2004@gmail.com>
2024-03-17 06:11:46 +0530

    Time: 1 ms (99.93%), Space: 49.3 MB (5.42%) - LeetHub

--- 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.java
+++ 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.java
@@ -16,12 +16,9 @@ public class Solution {
         while(temp1 != temp2) {
             temp1 = temp1.next;
             temp2 = temp2.next;
-            if(temp1 == temp2)
-                return temp1;
-            if(temp1 == null)
-                temp1 = headB;
-            if(temp2 == null)
-                temp2 = headA;
+            if(temp1 == temp2)  return temp1;
+            if(temp1 == null)   temp1 = headB;
+            if(temp2 == null)   temp2 = headA;
         }
         return temp1;
     }
