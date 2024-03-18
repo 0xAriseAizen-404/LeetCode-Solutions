64fb9bd8546d34210ab46da1eb0363a54828174f
Mahesh Avvaru <maheshavvaru2004@gmail.com>
2024-03-18 06:59:10 +0530

    Time: 275 ms (31.72%), Space: 95.3 MB (32.49%) - LeetHub

--- 0452-minimum-number-of-arrows-to-burst-balloons/0452-minimum-number-of-arrows-to-burst-balloons.cpp
+++ 0452-minimum-number-of-arrows-to-burst-balloons/0452-minimum-number-of-arrows-to-burst-balloons.cpp
@@ -1,26 +1,37 @@
 class Solution {
 public:
     int findMinArrowShots(vector<vector<int>>& points) {
+        // int n = points.size();
+        // if (n <= 1) 
+        //     return n;
+        // sort(points.begin(), points.end())
+        // int arrows = 1;
+        // int end = points[0][1];
+        // for (int i = 1; i < n; ++i) {
+        //     if (points[i][0] > end) {
+        //         arrows++;
+        //         end = points[i][1];
+        //     } else {
+        //         end = min(end, points[i][1]);
+        //     }
+        // }
+        // return arrows;
         int n = points.size();
-        
-        if (n == 0) {
-            return 0;
-        }
-        
+        if(n <= 1) // Changed to handle case of empty or single element vector
+            return n; // Return the size of input if empty or contains only one element
+
         sort(points.begin(), points.end());
-        
-        int arrows = 1;
-        int end = points[0][1];
-        
-        for (int i = 1; i < n; ++i) {
-            if (points[i][0] > end) {
-                arrows++;
-                end = points[i][1];
-            } else {
-                end = min(end, points[i][1]);
+        vector<vector<int>> res;
+
+        for(int i = 0; i < n; ++i) {
+            if(res.empty() || points[i][0] > res.back()[1]) {
+                res.push_back(points[i]);
+            }
+            else {
+                res.back()[1] = min(res.back()[1], points[i][1]);
             }
         }
-        
-        return arrows;
+
+        return res.size();
     }
 };
