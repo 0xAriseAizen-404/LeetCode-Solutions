9e6fa49cefa0526b7ea812eabbf51fd22025b5f2
Mahesh Avvaru <maheshavvaru2004@gmail.com>
2024-03-20 12:19:15 +0530

    Time: 16 ms (26.34%), Space: 42.1 MB (90.25%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.java
+++ 0205-isomorphic-strings/0205-isomorphic-strings.java
@@ -1,13 +1,25 @@
 class Solution {
-public:
-    bool isIsomorphic(string s, string t) {
-         unordered_map<char, char> mp, mp2;
-        for (int i=0; i<s.length(); ++i) {
-            if (mp[s[i]] && mp[s[i]]!=t[i]) return false;
-            if (mp2[t[i]] && mp2[t[i]]!=s[i]) return false;
-            mp[s[i]]=t[i];
-            mp2[t[i]]=s[i];
+    public boolean isIsomorphic(String s, String t) {
+        if (s.length() != t.length())
+            return false;
+        HashMap<Character, Character> sMap = new HashMap<>();
+        HashMap<Character, Character> tMap = new HashMap<>();
+        for (int i = 0; i < s.length(); ++i) {
+            char sChar = s.charAt(i);
+            char tChar = t.charAt(i);
+            if (sMap.containsKey(sChar)) {
+                if (sMap.get(sChar) != tChar)
+                    return false;
+            } else {
+                sMap.put(sChar, tChar);
+            }
+            if (tMap.containsKey(tChar)) {
+                if (tMap.get(tChar) != sChar)
+                    return false;
+            } else {
+                tMap.put(tChar, sChar);
+            }
         }
         return true;
     }
-};
+}
