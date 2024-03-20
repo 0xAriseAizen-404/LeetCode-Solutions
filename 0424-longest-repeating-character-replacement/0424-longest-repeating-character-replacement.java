8e992c7bb5a92cae135ccc29555cd55a5d0b849b
Mahesh Avvaru <maheshavvaru2004@gmail.com>
2024-03-20 06:45:38 +0530

    Time: 33 ms (18.61%), Space: 44.3 MB (28.58%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.java
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.java
@@ -7,19 +7,15 @@ class Solution {
         int right;
         int maxFreq = 0;
         int maxLength = 0;
-
         for (right = 0; right < s.length(); ++right) {
             hMap.put(s.charAt(right), hMap.getOrDefault(s.charAt(right), 0) + 1);
             maxFreq = Math.max(maxFreq, hMap.get(s.charAt(right)));
-
             if ((right - left + 1 - maxFreq) > k) {
                 hMap.put(s.charAt(left), hMap.get(s.charAt(left)) - 1);
                 left++;
             }
-
             maxLength = Math.max(maxLength, right - left + 1);
         }
-
         return maxLength;
     }
 }
