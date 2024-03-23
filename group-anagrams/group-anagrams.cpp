b1322d0f5c52055d2522cc35527f9106c2475008
Mahesh Avvaru <maheshavvaru2004@gmail.com>
2024-03-23 21:58:50 +0530

    Time: 23 ms, Memory: 23.8 MB - LeetHub

--- group-anagrams/group-anagrams.cpp
+++ group-anagrams/group-anagrams.cpp
@@ -1,16 +1,16 @@
 class Solution {
-    public List<List<String>> groupAnagrams(String[] strs) {
-        HashMap<String, ArrayList<String>> hmap = new HashMap<>();
-        for (String x : strs) {
-            char[] charArray = x.toCharArray();
-            Arrays.sort(charArray);
-            String sortedX = new String(charArray);
-            if (!hmap.containsKey(sortedX)) {
-                hmap.put(sortedX, new ArrayList<>());
-            }
-            hmap.get(sortedX).add(x);
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        unordered_map<string, vector<string>> umap;
+        for (string& x : strs) {
+            string origin = x;
+            sort(x.begin(), x.end());
+            umap[x].emplace_back(origin);
         }
-        List<List<String>> res = new ArrayList<>(hmap.values());
-        return res;
+        vector<vector<string>> result;
+        for (auto& entry : umap) {
+            result.push_back(entry.second);
+        }
+        return result;
     }
-}
+};
