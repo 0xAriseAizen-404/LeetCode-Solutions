2b855e121ba5969b33244429121448668616f0bc
Mahesh Avvaru <maheshavvaru2004@gmail.com>
2024-03-23 07:28:03 +0530

    Time: 0 ms, Memory: 8.5 MB - LeetHub

--- happy-number/happy-number.cpp
+++ happy-number/happy-number.cpp
@@ -1,19 +1,20 @@
 class Solution {
-    public boolean isHappy(int n) {
-        Map<Integer, Integer> myMap = new HashMap<>();
+public:
+    bool isHappy(int n) {
+        map<int, int> myMap;
         int num = n;
         while(num > 1) {
-            myMap.put(num, myMap.getOrDefault(num, 0) + 1);
-            if(myMap.get(num) > 1)
+            myMap[num]++;
+            if(myMap[num] > 1)
                 return false;
             int summ = 0;
             int rem = 0;
             while(num > 0) {
-                summ += Math.pow(num%10, 2);
+                summ += pow(num%10, 2);
                 num /= 10;
             }
             num = summ;
         }
         return num==1;
     }
-}
+};
