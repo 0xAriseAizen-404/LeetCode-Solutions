5df3cce116c9c130399520333e15ac2e0c48f902
Mahesh Avvaru <maheshavvaru2004@gmail.com>
2024-03-23 07:03:56 +0530

    Time: 73 ms (97.71%), Space: 112.6 MB (17.79%) - LeetHub

--- 0643-maximum-average-subarray-i/0643-maximum-average-subarray-i.cpp
+++ 0643-maximum-average-subarray-i/0643-maximum-average-subarray-i.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     double findMaxAverage(vector<int>& nums, int k) {
+        ios_base::sync_with_stdio(false); cin.tie(NULL);
         int windowOpening = 0;
         int sum = 0;
         int maxx = INT_MIN;
