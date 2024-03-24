7db893471ae74b61a4ac56747571d8f7f6c47671
Mahesh Avvaru <maheshavvaru2004@gmail.com>
2024-03-24 12:19:43 +0530

    Time: 9 ms, Memory: 45.3 MB - LeetHub

--- design-linked-list/design-linked-list.cpp
+++ design-linked-list/design-linked-list.cpp
@@ -1,66 +1,86 @@
-class MyLinkedList {
-private:
-    int size;
-    ListNode* dummy;
-
-public:
-    MyLinkedList() {
-        dummy = new ListNode(-1);
-        size = 0;
+class ListNode {
+    int value;
+    ListNode next;
+    ListNode(int value){
+        this.value = value;
+        this.next=null;
     }
+}
+class MyLinkedList {
+    int size;
+    ListNode dummy;
 
-    int get(int index) {
-        if (index >= size) {
+    public MyLinkedList() {
+        dummy=new ListNode(-1);
+        size=0;
+    }
+    
+    public int get(int index) {
+        if(index>=size){
             return -1;
         }
 
-        ListNode* cur = dummy;
-        for (int i = 0; i <= index; i++) {
-            cur = cur->next;
+        ListNode cur=dummy;
+        for(int i=0;i<=index;i++){
+            cur=cur.next;
         }
-        return cur->val;
+        return cur.value;
     }
+    
+    public void addAtHead(int val) {
 
-    void addAtHead(int val) {
-        ListNode* node = new ListNode(val);
-        node->next = dummy->next;
-        dummy->next = node;
+        ListNode node=new ListNode(val);
+        node.next=dummy.next;
+        dummy.next=node;
         size++;
     }
-
-    void addAtTail(int val) {
-        ListNode* node = new ListNode(val);
-        ListNode* cur = dummy;
-        for (int i = 0; i < size; i++) {
-            cur = cur->next;
+    
+    public void addAtTail(int val) {
+        ListNode node=new ListNode(val);
+        ListNode cur=dummy;
+        for(int i=0;i<size;i++){
+            cur=cur.next;
         }
-        cur->next = node;
+        cur.next=node;
         size++;
     }
+    
+    public void addAtIndex(int index, int val) {
+        
+        if(index>size) return;
 
-    void addAtIndex(int index, int val) {
-        if (index > size) return;
+        ListNode node=new ListNode(val);
+        ListNode cur=dummy;
 
-        ListNode* node = new ListNode(val);
-        ListNode* cur = dummy;
-
-        for (int i = 0; i < index; i++) {
-            cur = cur->next;
+        for(int i=0;i<index;i++){
+            cur=cur.next;
         }
-        node->next = cur->next;
-        cur->next = node;
+        node.next=cur.next;
+        cur.next=node;
         size++;
+
     }
+    
+    public void deleteAtIndex(int index) {
 
-    void deleteAtIndex(int index) {
-        if (index >= size) return;
+        if(index>=size) return;
+        ListNode cur=dummy;
 
-        ListNode* cur = dummy;
-
-        for (int i = 0; i < index; i++) {
-            cur = cur->next;
+        for(int i=0;i<index;i++){
+            cur=cur.next;
         }
-        cur->next = cur->next->next;
+        cur.next=cur.next.next;
         size--;
+        
     }
-};
+}
+
+/**
+ * Your MyLinkedList object will be instantiated and called as such:
+ * MyLinkedList obj = new MyLinkedList();
+ * int param_1 = obj.get(index);
+ * obj.addAtHead(val);
+ * obj.addAtTail(val);
+ * obj.addAtIndex(index,val);
+ * obj.deleteAtIndex(index);
+ */
