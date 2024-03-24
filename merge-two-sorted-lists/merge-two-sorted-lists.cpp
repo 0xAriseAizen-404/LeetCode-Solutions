a869648cf44c81835f65399f20074dc6365c1c85
Mahesh Avvaru <maheshavvaru2004@gmail.com>
2024-03-24 12:20:40 +0530

    Time: 4 ms, Memory: 18.3 MB - LeetHub

--- merge-two-sorted-lists/merge-two-sorted-lists.cpp
+++ merge-two-sorted-lists/merge-two-sorted-lists.cpp
@@ -1,29 +1,35 @@
 /**
  * Definition for singly-linked list.
- * public class ListNode {
+ * struct ListNode {
  *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- * }
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
  */
 class Solution {
-    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
-        ListNode dummy = new ListNode(0);
-        ListNode curr = dummy;
-        while (l1!=null && l2!=null) {
-            if(l1.val < l2.val) {
-                curr.next = l1;
-                l1 = l1.next;
+public:
+    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+        ListNode* dummy = new ListNode(0); // Create a dummy node
+        ListNode* curr = dummy; // Current pointer to traverse
+        // Merge the two lists
+        while (list1 != nullptr && list2 != nullptr) {
+            if (list1->val < list2->val) {
+                curr->next = list1;
+                list1 = list1->next;
             } else {
-                curr.next = l2;
-                l2 = l2.next;
+                curr->next = list2;
+                list2 = list2->next;
             }
-            curr = curr.next;
+            curr = curr->next;
         }
-        if (l1 != null) curr.next = l1;
-        if (l2 != null) curr.next = l2;
-        return dummy.next;
+        // Append the remaining nodes from the non-empty list
+        if (list1 != nullptr) {
+            curr->next = list1;
+        } else {
+            curr->next = list2;
+        }
+        return dummy->next; // Return the head of the merged list
     }
-}
+};
