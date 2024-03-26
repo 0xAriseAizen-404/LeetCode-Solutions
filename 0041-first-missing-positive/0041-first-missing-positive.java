a966de2b73105040f428708cd251b5c05a3f1ce4
Mahesh Avvaru <maheshavvaru2004@gmail.com>
2024-03-26 06:56:21 +0530

    Time: 1 ms (98.00%), Space: 54.9 MB (73.20%) - LeetHub

--- 0041-first-missing-positive/0041-first-missing-positive.java
+++ 0041-first-missing-positive/0041-first-missing-positive.java
@@ -1,12 +1,43 @@
+// class Solution {
+//     public int firstMissingPositive(int[] nums) {
+//         int n = nums.length;
+//         HashMap<Integer, Boolean> hMap = new HashMap<>();
+//         for (int ele : nums) hMap.put(ele, true);
+//         for (int i = 1; i <= n; ++i) {
+//             if (!hMap.containsKey(i)) // Corrected method name
+//                 return i;
+//         }
+//         return n + 1;
+//     }
+// }
+
 class Solution {
     public int firstMissingPositive(int[] nums) {
-        int n = nums.length;
-        HashMap<Integer, Boolean> hMap = new HashMap<>();
-        for (int ele : nums) hMap.put(ele, true);
-        for (int i = 1; i <= n; ++i) {
-            if (!hMap.containsKey(i)) // Corrected method name
-                return i;
+       int i=0;
+       while(i<nums.length){
+        int correct=nums[i]-1;
+        if(correct!=i && nums[i]>0 && nums[i]<nums.length){
+            if(nums[i]!=nums[nums[i]-1]){
+                swap(nums,i,correct);
+            }
+            else{
+                i++;
+            }
         }
-        return n + 1;
+        else{
+            i++;
+        }
+       }
+       for(int j=0;j<nums.length;j++){
+        if(nums[j]!=j+1){
+            return j+1;
+        }
+       }
+       return nums.length+1;
+    }
+    static void swap(int[] arr,int x,int y){
+        int temp=arr[x];
+        arr[x]=arr[y];
+        arr[y]=temp;
     }
 }
