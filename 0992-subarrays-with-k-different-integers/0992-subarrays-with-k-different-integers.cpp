4ebc32dfe7ed8c62fb3a089f004d44d587cecb55
Mahesh Avvaru <maheshavvaru2004@gmail.com>
2024-03-30 22:47:41 +0530

    Time: 113 ms (48.41%), Space: 49.8 MB (56.66%) - LeetHub

--- 0992-subarrays-with-k-different-integers/0992-subarrays-with-k-different-integers.cpp
+++ 0992-subarrays-with-k-different-integers/0992-subarrays-with-k-different-integers.cpp
@@ -1,29 +1,23 @@
 class Solution {
-    public int subarraysWithKDistinct(int[] nums, int k) {
-        int subWithMaxK = subarrayWithAtMostK(nums, k);
-        int reducedSubWithMaxK = subarrayWithAtMostK(nums, k - 1);
-        return subWithMaxK - reducedSubWithMaxK;
+public:
+    int subarraysWithKDistinct(vector<int>& nums, int k) {
+        int sub_with_max_element_k = subarray_with_atmost_k(nums,k);
+        int reduced_sub_with_max_k = subarray_with_atmost_k(nums,k-1);
+        return (sub_with_max_element_k - reduced_sub_with_max_k);
     }
-    
-    public int subarrayWithAtMostK(int[] nums, int k) {
-        HashMap<Integer, Integer> map = new HashMap<>();
-        int left = 0, right = 0, ans = 0;
-        
-        while (right < nums.length) {
-            map.put(nums[right], map.getOrDefault(nums[right], 0) + 1);
-            
-            while (map.size() > k) {
-                map.put(nums[left], map.get(nums[left]) - 1);
-                if (map.get(nums[left]) == 0) {
-                    map.remove(nums[left]);
-                }
+    int subarray_with_atmost_k(vector<int>& nums,int k){
+        unordered_map<int,int> map;
+        int left = 0 , right = 0,ans = 0;
+        while(right<nums.size()){
+            map[nums[right]]++;
+            while(map.size()>k){
+                map[nums[left]]--;
+                if(map[nums[left]]==0)map.erase(nums[left]);
                 left++;
             }
-            
-            ans += right - left + 1; // Size of subarray
+            ans += right-left+1; // basically the size of subarray;
             right++;
         }
-        
         return ans;
     }
-}
+};
