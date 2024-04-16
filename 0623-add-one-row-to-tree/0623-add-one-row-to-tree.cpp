4ef77452256af4a96923b6a0289ff593d1479a04
Mahesh Avvaru <maheshavvaru2004@gmail.com>
2024-04-16 21:52:54 +0530

    Time: 16 ms (32.77%), Space: 24.4 MB (49.35%) - LeetHub

--- 0623-add-one-row-to-tree/0623-add-one-row-to-tree.cpp
+++ 0623-add-one-row-to-tree/0623-add-one-row-to-tree.cpp
@@ -1,46 +1,45 @@
 /**
  * Definition for a binary tree node.
- * public class TreeNode {
+ * struct TreeNode {
  *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
  */
-public class Solution {
-    public TreeNode add(TreeNode root, int val, int depth, int curr) {
-        if (root == null)
-            return null;
+class Solution {
+public:
+    TreeNode* add(TreeNode* root, int val, int depth, int curr) {
+        if (!root)
+            return nullptr;
+
         if (curr == depth - 1) {
-            TreeNode lTemp = root.left;
-            TreeNode rTemp = root.right;
-            root.left = new TreeNode(val);
-            root.right = new TreeNode(val);
-            root.left.left = lTemp;
-            root.right.right = rTemp;
+            TreeNode* lTemp = root->left;
+            TreeNode* rTemp = root->right;
+
+            root->left = new TreeNode(val);
+            root->right = new TreeNode(val);
+            root->left->left = lTemp;
+            root->right->right = rTemp;
 
             return root;
         }
 
-        root.left = add(root.left, val, depth, curr + 1);
-        root.right = add(root.right, val, depth, curr + 1);
+        root->left = add(root->left, val, depth, curr + 1);
+        root->right = add(root->right, val, depth, curr + 1);
 
         return root;
     }
 
-    public TreeNode addOneRow(TreeNode root, int val, int depth) {
+    TreeNode* addOneRow(TreeNode* root, int val, int depth) {
         if (depth == 1) {
-            TreeNode newRoot = new TreeNode(val);
-            newRoot.left = root;
+            TreeNode* newRoot = new TreeNode(val);
+            newRoot->left = root;
             return newRoot;
         }
 
         return add(root, val, depth, 1);
     }
-}
+};
