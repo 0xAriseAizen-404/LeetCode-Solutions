b86e9507e9001efa2886b01630663482387a766f
Mahesh Avvaru <maheshavvaru2004@gmail.com>
2024-04-24 18:02:53 +0530

    Time: 0 ms (100.00%), Space: 40.1 MB (73.08%) - LeetHub

---
+++ 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.java
@@ -0,0 +1,51 @@
+// class Solution {
+//     public int tribonacci(int n) {
+//         if (n == 0) return 0;
+//         if (n == 1 || n == 2) return 1;
+//         return tribonacci(n-1)+tribonacci(n-2)+tribonacci(n-3);
+//     }
+// }
+
+class Solution {
+    public int tribonacci(int n) {
+        if(n==0){
+            return 0;
+        }
+        if(n==1 || n==2){
+            return 1;
+        }
+        int prev1=0,prev2=1,prev3=1,curr=0;
+        for(int i=3;i<=n;i++){
+            curr = prev1+prev2+prev3;
+            prev1 = prev2;
+            prev2 = prev3;
+            prev3 = curr;
+        }
+        return curr;
+    }
+}
+
+// class Solution {
+//     public int tribonacci(int n) {
+//         Map<Integer, Integer> memo = new HashMap<>();
+//         return helper(n, memo);
+//     }
+    
+//     private int helper(int n, Map<Integer, Integer> memo) {
+//         if (memo.containsKey(n)) {
+//             return memo.get(n);
+//         }
+        
+//         int result;
+//         if (n == 0) {
+//             result = 0;
+//         } else if (n == 1 || n == 2) {
+//             result = 1;
+//         } else {
+//             result = helper(n - 1, memo) + helper(n - 2, memo) + helper(n - 3, memo);
+//         }
+        
+//         memo.put(n, result);
+//         return result;
+//     }
+// }
