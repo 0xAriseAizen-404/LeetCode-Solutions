a433118e6d2397a870a987bcfeafb6bcfc60892f
Mahesh Avvaru <maheshavvaru2004@gmail.com>
2024-05-08 05:42:36 +0530

    Time: 17 ms (22.17%), Space: 53.2 MB (79.88%) - LeetHub

--- 0881-boats-to-save-people/0881-boats-to-save-people.java
+++ 0881-boats-to-save-people/0881-boats-to-save-people.java
@@ -1,13 +1,15 @@
 class Solution {
-public:
-    int numRescueBoats(vector<int>& people, int limit) {
-        sort(people.begin(),people.end());
-        int i = 0, j = people.size()-1, count = 0;
+    public int numRescueBoats(int[] people, int limit) {
+        int boats = 0;
+        Arrays.sort(people);
+        int i=0,j=people.length-1;
         while(i<=j){
-            if(people[i] + people[j] <= limit)i++;
-            count++;
+            if((people[j]+people[i])<=limit){
+                i++;
+            }
             j--;
+            boats++;
         }
-        return count;
+        return boats;
     }
-};
+}
