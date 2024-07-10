5e591fe21606b319e3a8859df56841c2b1d24f10
Mahesh Avvaru <maheshavvaru2004@gmail.com>
2024-07-10 09:08:04 +0530

    Time: 4 ms (67.13%), Space: 14 MB (61.11%) - LeetHub

---
+++ 1720-crawler-log-folder/1720-crawler-log-folder.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int minOperations(vector<string>& logs) {
+        int distance = 0;
+        for (string x: logs) {
+            if (x == "./") continue;
+            else if (x == "../") {
+                if (distance > 0) distance--;
+            }
+            else distance++;
+        }
+        return distance;
+    }
+};
