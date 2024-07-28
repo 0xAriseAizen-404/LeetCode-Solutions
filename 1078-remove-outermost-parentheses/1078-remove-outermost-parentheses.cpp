8584c9d5b71a6ac78826cf5f981ecda7f24b2d94
Mahesh Avvaru <maheshavvaru2004@gmail.com>
2024-07-28 16:56:14 +0530

    Time: 2 ms (70.65%), Space: 8.2 MB (38.27%) - LeetHub

---
+++ 1078-remove-outermost-parentheses/1078-remove-outermost-parentheses.cpp
@@ -0,0 +1,50 @@
+class Solution {
+public:
+    string removeOuterParentheses(string S) {
+        string result;
+        int openCount = 0;
+
+        for (char c : S) {
+            if (c == '(') {
+                if (openCount > 0) {
+                    result += c;
+                }
+                openCount++;
+            } else {
+                openCount--;
+                if (openCount > 0) {
+                    result += c;
+                }
+            }
+        }
+
+        return result;
+    }
+};
+
+
+// class Solution {
+// public:
+//     string removeOuterParentheses(string paranthesis) {
+//         stack<int> st;
+//         string result = "";
+//         for (char x: paranthesis) {
+//             if (x == '(') st.push(x);
+//             else {
+//                 if (st.size() == 0) result += x;
+//                 if (st.size() == 1) {
+//                     st.pop();
+//                     continue;
+//                 }
+//                 else {
+//                     while (st.size() != 1) {
+//                         result += st.top();
+//                         st.pop();
+//                     }
+//                     result += x;
+//                 }
+//             }
+//         }
+//         return result;
+//     }
+// };
