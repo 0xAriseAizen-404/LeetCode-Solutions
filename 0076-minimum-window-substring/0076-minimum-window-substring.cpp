48563518c32953b66eb752617efe14ce92981232
Mahesh Avvaru <maheshavvaru2004@gmail.com>
2024-08-01 21:33:05 +0530

    Time: 27 ms (39.23%), Space: 11.2 MB (15.86%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.cpp
+++ 0076-minimum-window-substring/0076-minimum-window-substring.cpp
@@ -1,40 +1,39 @@
 class Solution {
 public:
     string minWindow(string s, string t) {
-        unordered_map<char, int> map;
-        for (char x : t) 
-            map[x]++;
-        
-        int matched = 0;
-        int startInd = 0;
-        int subStrStartInd = 0;
-        int minLen = s.size() + 1;
-        
-        for (int endInd = 0; endInd < s.size(); endInd++) {
-            char x = s[endInd];
-            if (map.find(x) != map.end()) {
-                map[x]--;
-                if (map[x] == 0)
-                    matched += 1;
+        if (s.size() == 0 || t.size() == 0 || s.size() < t.size()) return "";
+
+        unordered_map<char, int> mapp;
+        for (char x : t) mapp[x]++;
+
+        int required = mapp.size();
+        int left = 0, right = 0;
+        int got = 0;
+        int ans[] = {-1, 0, 0};
+
+        unordered_map<char, int> subStringMap;
+        while (right < s.size()) {
+            char x = s[right];
+            subStringMap[x]++;
+            if (mapp.find(x) != mapp.end() && mapp[x] == subStringMap[x]) {
+                got++;
             }
-            
-            while (matched == map.size()) {
-                if (minLen > (endInd - startInd + 1)) {
-                    minLen = endInd - startInd + 1;
-                    subStrStartInd = startInd;
+            while (left <= right && required == got) {
+                x = s[left];
+                if (ans[0] == -1 || ans[0] >= (right - left + 1)) {
+                    ans[0] = right - left + 1;
+                    ans[1] = left;
+                    ans[2] = right;
                 }
-                
-                char deleted = s[startInd];
-                if (map.find(deleted) != map.end()) {
-                    if (map[deleted] == 0) 
-                        matched--;
-                    map[deleted]++;
+                subStringMap[x]--;
+                if (mapp.find(x) != mapp.end() && mapp[x] > subStringMap[x]) {
+                    got--;
                 }
-                
-                startInd++;
+                left++;
             }
+            right++;
         }
-        
-        return minLen > s.size() ? "" : s.substr(subStrStartInd, minLen);
+        if (ans[0] == -1) return "";
+        return s.substr(ans[1], ans[0]);
     }
 };
