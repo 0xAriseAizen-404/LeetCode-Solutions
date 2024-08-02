9e548c5c40378357d4dedc2df5c984b03cfe8eb6
Mahesh Avvaru <maheshavvaru2004@gmail.com>
2024-08-02 17:34:06 +0530

    Time: 3 ms (58.21%), Space: 9.1 MB (27.15%) - LeetHub

--- 0008-string-to-integer-atoi/0008-string-to-integer-atoi.cpp
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.cpp
@@ -10,19 +10,16 @@ public:
         while (ind < s.size() && isspace(s[ind])) {
             ind++;
         }
-
         if (ind < s.size() && (s[ind] == '+' || s[ind] == '-')) {
             isNegative = (s[ind] == '-');
             ind++;
         }
-
         while (ind < s.size() && isdigit(s[ind])) {
             int digit = s[ind] - '0';
-
+            // overflow of value
             if (integerVal > (INT_MAX - digit) / 10) {
                 return isNegative ? INT_MIN : INT_MAX;
             }
-
             integerVal = integerVal * 10 + digit;
             ind++;
         }
