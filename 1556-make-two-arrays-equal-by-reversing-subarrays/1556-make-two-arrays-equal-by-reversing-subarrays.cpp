5dfa6b3b6d6e4ce9c4de1ba46d6ea19ea4d5bacd
Mahesh Avvaru <maheshavvaru2004@gmail.com>
2024-08-03 12:17:19 +0530

    Time: 4 ms (92.55%), Space: 17.3 MB (62.74%) - LeetHub

--- 1556-make-two-arrays-equal-by-reversing-subarrays/1556-make-two-arrays-equal-by-reversing-subarrays.cpp
+++ 1556-make-two-arrays-equal-by-reversing-subarrays/1556-make-two-arrays-equal-by-reversing-subarrays.cpp
@@ -1,13 +1,10 @@
-#include <vector>
-#include <cstdlib> 
-
 class Solution {
 public:
-    static int compare(const void* a, const void* b) {
-        return (*(int*)a - *(int*)b);
+    static int compare (const void * a, const void * b)
+    {
+    return ( *(int*)a - *(int*)b );
     }
-
-    bool canBeEqual(std::vector<int>& target, std::vector<int>& arr) {
+    bool canBeEqual(vector<int>& target, vector<int>& arr) {
         qsort(target.data(), target.size(), sizeof(int), compare);
         qsort(arr.data(), arr.size(), sizeof(int), compare);
         return target == arr;
