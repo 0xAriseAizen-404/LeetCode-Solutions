2055a1f03f62235bc26e78afa8bbab97d0a0ca74
Mahesh Avvaru <maheshavvaru2004@gmail.com>
2024-08-06 14:25:53 +0530

    Time: 5 ms (82.07%), Space: 21.4 MB (32.28%) - LeetHub

---
+++ 0647-palindromic-substrings/0647-palindromic-substrings.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int checkPalindrome(string s, int left, int right) {
+        int count = 0;
+        while (left>=0 && right<=s.size() && s[left]==s[right]) {
+            count++;
+            left--;
+            right++;
+        }
+        return count;
+    }
+    int countSubstrings(string s) {
+        if (s.empty()) return 0;
+
+        int count = 0;
+        for (int i=0; i<s.size(); ++i) {
+            count += checkPalindrome(s, i, i);
+            count += checkPalindrome(s, i, i+1);
+        }
+        return count;
+    }
+};
