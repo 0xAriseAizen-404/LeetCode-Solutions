1df1080c8264bf43399ae1cc84f8f3a410be2714
Mahesh Avvaru <maheshavvaru2004@gmail.com>
2024-08-22 16:44:55 +0530

    Time: 3 ms (96.01%), Space: 8.4 MB (77.11%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
@@ -2,20 +2,16 @@ class Solution {
 public:
     int characterReplacement(string s, int k) {
         int *chars = new int[26]();
-        
-        int left = 0, right = 0;
-        int maxLength = 0;
-        int maxxFreq = 0;
-
-        while (right < s.size()) {
-            maxxFreq = max(maxxFreq, ++chars[s[right] - 'A']);
-            if ((right-left+1) - maxxFreq > k) {
-                chars[s[left] - 'A']--;
-                left++;
-            }
-            maxLength = max(maxLength, right-left+1);
+        int left = 0;
+        int right = 0;
+        int maxFreq = 0;
+        int maxLen = 0;
+        while (right < s.length()) {
+            maxFreq = max(maxFreq, ++chars[s[right] - 'A']);
+            if ((right-left+1) - maxFreq > k) --chars[s[left++] - 'A'];
+            maxLen = max(maxLen, right-left+1);
             right++;
         }
-        return maxLength;
+        return maxLen;
     }
 };
