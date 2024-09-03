43ba127382264e6de52c58ecb4dc6494cf8e58ed
Mahesh Avvaru <maheshavvaru2004@gmail.com>
2024-09-03 21:07:37 +0530

    Time: 161 ms (81.53%), Space: 137.3 MB (65.44%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
@@ -1,15 +1,45 @@
 class Solution {
 public:
-    vector<int> maxSlidingWindow(vector<int>& a, int k) {
-        priority_queue<pair<int , int>> pq;
-        vector<int>ans;
-        int i = 0 , j = 0;
-        while(j < a.size()) {
-            while(j - i < k) pq.push({a[j] , j++});
-            while(!pq.empty() and j - pq.top().second > k) pq.pop();
-            ans.emplace_back(pq.top().first);
-            i++;
+    vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+        vector<int> res;
+        deque<int> dq;
+        
+        for (int i = 0; i < nums.size(); ++i) {
+            // Remove index of element not within the sliding window
+            if (!dq.empty() && dq.front() == i - k) {
+                dq.pop_front();
+            }
+            while (!dq.empty() && nums[dq.back()] <= nums[i]) {
+                dq.pop_back();
+            }
+            dq.push_back(i);
+            if (i >= k - 1) {
+                res.push_back(nums[dq.front()]);
+            }
         }
-        return ans;
+        
+        return res;
     }
-}; 
+};
+
+
+// class Solution {
+// public:
+//     vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+//         vector<int> res;
+//         deque<int> dq;
+//         for (int i=0; i<k; ++i) {
+//             while (!dq.empty() && dq.front() < nums[i]) dq.pop_front();
+//             dq.push_front(nums[i]);
+//         }
+//         res.push_back(dq.front());
+//         int left = 0;
+//         for (int i=k; i<nums.size(); ++i) {
+//             dq.erase(nums[left]);
+//             while (dq.front() < nums[i]) dq.pop_front();
+//             dq.push_front(nums[i]);
+//             res.push_back(dq.front());
+//         }
+//         return res;
+//     }
+// };
