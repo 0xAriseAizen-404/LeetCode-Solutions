21b7db1a9ede2c0884169e7d01658b9b1b0e04d7
Mahesh Avvaru <maheshavvaru2004@gmail.com>
2024-09-04 18:06:31 +0530

    Time: 4 ms (72.83%), Space: 12.2 MB (14.87%) - LeetHub

--- 0071-simplify-path/0071-simplify-path.cpp
+++ 0071-simplify-path/0071-simplify-path.cpp
@@ -5,18 +5,18 @@ public:
         stringstream ss(path);
         string in;
         while (getline(ss, in, '/')) {
-            if (!st.empty() && in == ".") continue;
-            else if (st.empty() && in == ".") continue;
-            else if (!st.empty() && in == "..") st.pop();
-            else if (st.empty() && in == "..") continue;
-            else if (in == "") continue;
-            else st.push(in);
+            if (in == "." || in == "") continue;
+            if (in == "..") {
+                if (!st.empty()) st.pop();
+            } else {
+                st.push(in);
+            }
         }
         string res = "";
         while (!st.empty()) {
             res = "/" + st.top() + res;
             st.pop();
         }
-        return res=="" ? "/" : res;
+        return res.empty() ? "/" : res;
     }
 };
