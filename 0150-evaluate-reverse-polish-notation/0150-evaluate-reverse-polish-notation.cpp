0d6c0c133b313ddcd2a1826a90e408cfdcbb0b7d
Mahesh Avvaru <maheshavvaru2004@gmail.com>
2024-09-08 10:35:51 +0530

    Time: 9 ms (35.9%), Space: 15.6 MB (81.99%) - LeetHub

---
+++ 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.cpp
@@ -0,0 +1,52 @@
+class Solution {
+private:
+    bool isOperator (string &x) {
+        return x == "+" || x == "-" || x == "/" || x == "%" || x == "*" || x == "^";
+    }
+    int myHelper (int op1, int op2, string &x) {
+        int res = 0;
+        switch (x[0]) {
+            case '+':
+                res = op1 + op2;
+                break;
+            case '-':
+                res = op1 - op2;
+                break;
+            case '/':
+                res = op1 / op2;
+                break;
+            case '*':
+                res = op1 * op2;
+                break;
+            case '%':
+                res = op1 % op2;
+                break;
+            case '^':
+                res = op1 ^ op2;
+                break;
+        }
+        return res;
+    }
+public:
+    int evalRPN(vector<string>& tokens) {
+        stack<int> st;
+        int op1 = 0;
+        int op2 = 0;
+        for (string &x: tokens) {
+            if (isOperator(x)) {
+                op1 = st.top(); st.pop();
+                op2 = st.top(); st.pop();
+                swap(op1, op2);
+                // cout << op1 << " " << op2 << " " << x << endl;
+                st.push(myHelper(op1, op2, x));
+            } else {
+                stringstream ss;
+                int num = 0;
+                ss << x; ss >> num;
+                st.push(num);
+                // st.push(stoi(x));
+            }
+        }
+        return st.top();
+    }
+};
