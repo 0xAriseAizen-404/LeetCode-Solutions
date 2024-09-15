03e4f5768683416674edba1b45a83bf6a2bc6a93
Mahesh Avvaru <maheshavvaru2004@gmail.com>
2024-09-15 10:25:29 +0530

    Time: 14 ms (83.85%), Space: 14 MB (55.05%) - LeetHub

--- 0227-basic-calculator-ii/0227-basic-calculator-ii.cpp
+++ 0227-basic-calculator-ii/0227-basic-calculator-ii.cpp
@@ -1,124 +1,51 @@
 class Solution {
-private:
-    string myHelper(string str) {
-        string operators = "+-%/^";
-        string exp = "";
-        for (int i = 0; i < str.size(); ++i) {
-            if (str[i] == ' ')
-                continue;
-            else if (operators.find(str[i]) != string::npos) {
-                if ((exp.empty() || exp.back() == '(') && str[i] == '-') {
-                    exp += '0';
-                }
-                exp += str[i];
-            } else {
-                exp += str[i];
+public:
+    int calculate(string s) {
+        stack<int> st;
+        int num = 0;
+        char prevOperator = '+';
+
+        for (int i = 0; i <= s.length(); i++) {
+            char ch = (i < s.length()) ? s[i] : '\0';
+
+            if (isdigit(ch)) {
+                num = num * 10 + (ch - '0');
             }
-        }
-        return exp;
-    }
 
-    bool isOperator(char x) {
-        return x == '+' || x == '-' || x == '*' || x == '%' || x == '/' ||
-               x == '^';
-    }
-
-    int getPriority(char x) {
-        switch (x) {
-        case '+':
-        case '-':
-            return 1;
-        case '*':
-        case '/':
-        case '%':
-            return 2;
-        case '^':
-            return 3;
-        default:
-            return 0;
-        }
-    }
-
-    vector<string> convertInfixToPostfix(string exp) {
-        vector<string> res;
-        stack<char> st;
-        string num = "";
-
-        for (char& x : exp) {
-            if (isalnum(x)) {
-                num += x;
-            } else {
-                if (!num.empty()) {
-                    res.push_back(num);
-                    num.clear();
+            if ((!isdigit(ch) && ch != ' ') || i == s.length()) {
+                if (prevOperator == '+') st.push(num);
+                if (prevOperator == '-') st.push(-num);
+                if (prevOperator == '*') {
+                    int temp = st.top() * num;
+                    st.pop();
+                    st.push(temp);
+                }
+                if (prevOperator == '/') {
+                    int temp = st.top() / num;
+                    st.pop();
+                    st.push(temp);
                 }
 
-                if (x == '(') {
-                    st.push(x);
-                } else if (x == ')') {
-                    while (!st.empty() && st.top() != '(') {
-                        res.push_back(string(1, st.top()));
-                        st.pop();
-                    }
-                    st.pop(); // Pop the '('
-                } else if (isOperator(x)) {
-                    while (!st.empty() &&
-                           getPriority(st.top()) >= getPriority(x)) {
-                        res.push_back(string(1, st.top()));
-                        st.pop();
-                    }
-                    st.push(x);
-                }
+                prevOperator = ch;
+                num = 0;
             }
         }
 
-        if (!num.empty()) {
-            res.push_back(num);
-        }
-
+        int result = 0;
         while (!st.empty()) {
-            res.push_back(string(1, st.top()));
+            result += st.top();
             st.pop();
         }
 
-        return res;
-    }
-
-    int evaluatePostfix(vector<string> A) {
-        stack<int> stk;
-
-        for (auto& token : A) {
-            if (token == "+" || token == "-" || token == "*" || token == "/" ||
-                token == "%" || token == "^") {
-                int b = stk.top();
-                stk.pop();
-                int a = stk.top();
-                stk.pop();
-
-                if (token == "+")
-                    stk.push(a + b);
-                else if (token == "-")
-                    stk.push(a - b);
-                else if (token == "*")
-                    stk.push(a * b);
-                else if (token == "/")
-                    stk.push(a / b);
-                else if (token == "%")
-                    stk.push(a % b);
-                else if (token == "^")
-                    stk.push(pow(a, b));
-            } else {
-                stk.push(stoi(token));
-            }
-        }
-        return stk.top();
-    }
-
-public:
-    int calculate(string s) {
-        string expression = myHelper(s);
-        // cout << expression << endl;
-        vector<string> postFix = convertInfixToPostfix(expression);
-        return evaluatePostfix(postFix);
+        return result;
     }
 };
+
+// Usage Example
+// int main() {
+//     Solution sol;
+//     string expression = "3+2*2";
+//     int result = sol.calculate(expression);
+//     cout << "Result: " << result << endl; // Output: 7
+//     return 0;
+// }
