32c3f50b29715240a0bde823c015d87a3e89e2b1
Mahesh Avvaru <maheshavvaru2004@gmail.com>
2024-09-18 16:28:27 +0530

    Time: 64 ms (35.36%), Space: 45.6 MB (47.87%) - LeetHub

--- 0012-integer-to-roman/0012-integer-to-roman.cs
+++ 0012-integer-to-roman/0012-integer-to-roman.cs
@@ -1,32 +1,15 @@
-using System;
-using System.Collections.Generic;
-
 public class Solution {
     public string IntToRoman(int num) {
-        var romans = new Tuple<int, string>[]
-        {
-            Tuple.Create(1000, "M"),
-            Tuple.Create(900, "CM"),
-            Tuple.Create(500, "D"),
-            Tuple.Create(400, "CD"),
-            Tuple.Create(100, "C"),
-            Tuple.Create(90, "XC"),
-            Tuple.Create(50, "L"),
-            Tuple.Create(40, "XL"),
-            Tuple.Create(10, "X"),
-            Tuple.Create(9, "IX"),
-            Tuple.Create(5, "V"),
-            Tuple.Create(4, "IV"),
-            Tuple.Create(1, "I")
-        };
+        var ret = new StringBuilder("");
+        string[] thousands = {"","M","MM","MMM"};
+        string[] hundreds = {"","C","CC","CCC", "CD","D","DC","DCC","DCCC","CM"};
+        string[] tens = {"","X","XX","XXX", "XL","L","LX", "LXX","LXXX", "XC"};
+        string[] single = {"","I","II","III","IV","V","VI", "VII", "VIII", "IX"};
 
-        string res = "";
-        foreach (var roman in romans) {
-            while (num >= roman.Item1) {
-                res += roman.Item2;
-                num -= roman.Item1;
-            }
-        }
-        return res;
+        ret.Append(thousands[num/1000]);
+        ret.Append(hundreds[(num % 1000) / 100]);
+        ret.Append(tens[(num % 100) / 10]);
+        ret.Append(single[num % 10]);
+        return ret.ToString();
     }
 }
