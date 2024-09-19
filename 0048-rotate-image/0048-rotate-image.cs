dbb5202f56dda54e1ffda02e15b18ba37453b45e
Mahesh Avvaru <maheshavvaru2004@gmail.com>
2024-09-19 10:49:10 +0530

    Time: 97 ms (79.06%), Space: 45.4 MB (92.08%) - LeetHub

---
+++ 0048-rotate-image/0048-rotate-image.cs
@@ -0,0 +1,15 @@
+public class Solution {
+    public void Rotate(int[][] mat) {
+        int n = mat.GetLength(0);
+        for (int i = 0; i < n; ++i) {
+            for (int j = i + 1; j < n; ++j) {
+                mat[i][j] ^= mat[j][i];
+                mat[j][i] ^= mat[i][j];
+                mat[i][j] ^= mat[j][i];
+            }
+        }
+        for (int i = 0; i < n; ++i) {
+            Array.Reverse(mat[i]);
+        }
+    }
+}
