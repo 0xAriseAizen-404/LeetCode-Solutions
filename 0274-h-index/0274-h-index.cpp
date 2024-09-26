62cd7490968473a043992d2d0fb3e0420aa768d9
Mahesh Avvaru <maheshavvaru2004@gmail.com>
2024-09-26 11:25:10 +0530

    Time: 0 ms (100%), Space: 11.1 MB (71.44%) - LeetHub

--- 0274-h-index/0274-h-index.cpp
+++ 0274-h-index/0274-h-index.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     int hIndex(vector<int>& citations) {
         sort(citations.begin(), citations.end(), greater<int>());
-        int h =0;
+        int h = 0;
         for (int i=0;i<=citations.size()-1;i++){
             if(i<citations[i]){
                 h++;
