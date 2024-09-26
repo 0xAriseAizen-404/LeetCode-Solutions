7c7515c1c81bcba075089643b3b7127b018a003c
Mahesh Avvaru <maheshavvaru2004@gmail.com>
2024-09-26 11:24:56 +0530

    Time: 6 ms (19.28%), Space: 11 MB (71.44%) - LeetHub

--- 0274-h-index/0274-h-index.cpp
+++ 0274-h-index/0274-h-index.cpp
@@ -1,20 +1,15 @@
 class Solution {
-private:
-    bool isValid(vector<int> &citations, int V) {
-        return (citations.size() - V) <= citations[V];
-    }
 public:
     int hIndex(vector<int>& citations) {
-        sort(citations.begin(), citations.end());
-        int low = 0, high = citations.size() - 1;
-        while (low <= high) {
-            int mid = low + (high - low) / 2;
-            if (isValid(citations, mid)) {
-                high = mid - 1;
-            } else {
-                low = mid + 1;
+        sort(citations.begin(), citations.end(), greater<int>());
+        int h =0;
+        for (int i=0;i<=citations.size()-1;i++){
+            if(i<citations[i]){
+                h++;
             }
+            else
+                return h;
         }
-        return citations.size() - low;
+        return h;
     }
 };
