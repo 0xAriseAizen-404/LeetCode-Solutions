3e99fc656007c1d5f7c7e636de3aa50a4c07d4b4
Mahesh Avvaru <maheshavvaru2004@gmail.com>
2024-09-29 14:17:11 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 2323-minimum-bit-flips-to-convert-number/2323-minimum-bit-flips-to-convert-number.cpp
+++ 2323-minimum-bit-flips-to-convert-number/2323-minimum-bit-flips-to-convert-number.cpp
@@ -1,6 +1,12 @@
 class Solution {
 public:
     int minBitFlips(int start, int goal) {
-        return __builtin_popcount(start ^ goal);
+        // return __builtin_popcount(start ^ goal);
+        int notEqualBits_cnt = 0;
+        for (int i=0; i<32; ++i) {
+            if ((start&(1 << i)) != (goal&&(1 << i)))
+                notEqualBits_cnt += 1;
+        }
+        return notEqualBits_cnt;
     }
 };
