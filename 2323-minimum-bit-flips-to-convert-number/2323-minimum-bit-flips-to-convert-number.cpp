8be6fd1a2df9beadbb77b2b33e69a7e5c95e0a12
Mahesh Avvaru <maheshavvaru2004@gmail.com>
2024-09-29 14:17:33 +0530

    Time: 0 ms (100%), Space: 7.6 MB (79.16%) - LeetHub

--- 2323-minimum-bit-flips-to-convert-number/2323-minimum-bit-flips-to-convert-number.cpp
+++ 2323-minimum-bit-flips-to-convert-number/2323-minimum-bit-flips-to-convert-number.cpp
@@ -4,7 +4,7 @@ public:
         // return __builtin_popcount(start ^ goal);
         int notEqualBits_cnt = 0;
         for (int i=0; i<32; ++i) {
-            if ((start&(1 << i)) != (goal&&(1 << i)))
+            if ((start&(1 << i)) != (goal&(1 << i)))
                 notEqualBits_cnt += 1;
         }
         return notEqualBits_cnt;
