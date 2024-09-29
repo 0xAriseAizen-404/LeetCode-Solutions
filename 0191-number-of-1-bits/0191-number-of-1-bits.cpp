b156f5c309b40cdd77c7eef419fa64a6f712fa45
Mahesh Avvaru <maheshavvaru2004@gmail.com>
2024-09-29 17:13:35 +0530

    Time: 2 ms (37.33%), Space: 7.8 MB (91.79%) - LeetHub

--- 0191-number-of-1-bits/0191-number-of-1-bits.cpp
+++ 0191-number-of-1-bits/0191-number-of-1-bits.cpp
@@ -1,10 +1,12 @@
 class Solution {
 public:
     int hammingWeight(int n) {
-        int cnt = 0;
-        for (int i=0; i<32; ++i) {
-            if (n & (1 << i)) cnt++;
-        }
-        return cnt;
+        // int cnt = 0;
+        // for (int i=0; i<32; ++i) {
+        //     if (n & (1 << i)) cnt++;
+        // }
+        // return cnt;
+
+        return __builtin_popcount(n);
     }
 };
