6baae9bdb6ab39de1cf6c6e3f022bbd5b2878635
Mahesh Avvaru <maheshavvaru2004@gmail.com>
2024-09-29 17:16:22 +0530

    Time: 2 ms (37.33%), Space: 7.9 MB (36.02%) - LeetHub

--- 0191-number-of-1-bits/0191-number-of-1-bits.cpp
+++ 0191-number-of-1-bits/0191-number-of-1-bits.cpp
@@ -9,11 +9,21 @@ public:
 
         // return __builtin_popcount(n);
 
+        // int count = 0;
+        // while (n != 0) {
+        //     n = n & (n - 1);
+        //     count++;
+        // }
+        // return count;
+
         int count = 0;
-        while (n != 0) {
-            n = n & (n - 1);
-            count++;
+        while (n > 0) {
+            if ((n & 1) == 1) {
+                count++;
+            }
+            n >>= 1;
         }
+
         return count;
     }
 };
