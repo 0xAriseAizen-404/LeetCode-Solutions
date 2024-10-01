83c6be81355a9059a9ee60a86f59b9d03ecc9334
Mahesh Avvaru <maheshavvaru2004@gmail.com>
2024-10-01 17:40:49 +0530

    Time: 15 ms (5.73%), Space: 18.3 MB (60.95%) - LeetHub

--- 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.cpp
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.cpp
@@ -1,12 +1,23 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        unordered_map<int, int> umap;
-        for (int i=0; i<nums.size(); ++i) {
-            if (umap.find(target - nums[i]) != umap.end()) 
-                return {umap[(target - nums[i])], i + 1};
-            umap[nums[i]] = i + 1;
+
+        int idx_low = 0;
+        int idx_high = nums.size() - 1;
+
+        while(nums[idx_low] + nums[idx_high] != target) {
+        if(nums[idx_low] + nums[idx_high] > target) --idx_high;
+        else ++idx_low;
         }
-        return {-1, -1};
+
+        return {idx_low + 1, idx_high + 1};
+
+        // unordered_map<int, int> umap;
+        // for (int i=0; i<nums.size(); ++i) {
+        //     if (umap.find(target - nums[i]) != umap.end()) 
+        //         return {umap[(target - nums[i])], i + 1};
+        //     umap[nums[i]] = i + 1;
+        // }
+        // return {-1, -1};
     }
 };
