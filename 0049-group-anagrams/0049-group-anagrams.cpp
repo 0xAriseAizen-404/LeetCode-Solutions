9fe87e7b6f0211a02f65ffcf41058b17bc2e1027
Mahesh Avvaru <maheshavvaru2004@gmail.com>
2024-10-01 21:05:02 +0530

    Time: 41 ms (18.48%), Space: 26.1 MB (23.1%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.cpp
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -2,15 +2,18 @@ class Solution {
 public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
         unordered_map<string, vector<string>> umap;
-        for (string& x : strs) {
-            string origin = x;
-            sort(x.begin(), x.end());
-            umap[x].emplace_back(origin);
+        vector<int> hash;
+        for (const auto &str: strs) {
+            hash.assign(26, 0);
+            for (const auto &x: str) {
+                hash[tolower(x) - 'a']++;
+            }
+            string encode = "";
+            for (const auto &x: hash) encode += x;
+            umap[encode].push_back(str);
         }
-        vector<vector<string>> result;
-        for (auto& entry : umap) {
-            result.push_back(entry.second);
-        }
-        return result;
+        vector<vector<string>> res;
+        for (const auto &x: umap) res.push_back(x.second);
+        return res;
     }
 };
