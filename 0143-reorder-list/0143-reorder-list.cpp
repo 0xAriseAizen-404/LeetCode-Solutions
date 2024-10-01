d60553145df2b0c24530d6c7ed848ed646abf666
Mahesh Avvaru <maheshavvaru2004@gmail.com>
2024-10-01 22:39:34 +0530

    Time: 35 ms (11.55%), Space: 22.7 MB (49.92%) - LeetHub

--- 0143-reorder-list/0143-reorder-list.cpp
+++ 0143-reorder-list/0143-reorder-list.cpp
@@ -9,65 +9,41 @@
  * };
  */
 class Solution {
+private:
+    ListNode* findMiddle(ListNode* head) {
+        ListNode* prev = nullptr;
+        while (head && head->next) {
+            prev = (prev == nullptr) ? head->next : prev->next;
+            head = head->next->next;
+        }
+        return prev;
+    }
+    
+    ListNode* reverse(ListNode* head) {
+        ListNode* prev = nullptr;
+        while (head) {
+            ListNode* next = head->next;
+            head->next = prev;
+            prev = head;
+            head = next;
+        }
+        return prev;
+    }
+    
 public:
     void reorderList(ListNode* head) {
-        if (!head || !head->next || !head->next->next) {
-            return;
-        }
-
-        // Step 1: Find the middle of the list
-        ListNode *slow = head;
-        ListNode *fast = head;
-
-        while (fast->next && fast->next->next) {
-            slow = slow->next;
-            fast = fast->next->next;
-        }
-
-        // Step 2: Reverse the second half of the list
-        ListNode *prev = nullptr;
-        ListNode *curr = slow->next;
-        slow->next = nullptr;
-
-        while (curr) {
-            ListNode *nextNode = curr->next;
-            curr->next = prev;
-            prev = curr;
-            curr = nextNode;
-        }
-
-        // Step 3: Merge the first half and the reversed second half
-        ListNode *first = head;
-        ListNode *second = prev;
-
+        if (!head || !head->next) return;
+        ListNode* mid = findMiddle(head);
+        ListNode* second = reverse(mid->next);
+        ListNode* first = head;
+        mid->next = nullptr;
         while (second) {
-            ListNode *nextFirst = first->next;
-            ListNode *nextSecond = second->next;
+            ListNode* tmp1 = first->next;
+            ListNode* tmp2 = second->next;
             first->next = second;
-            second->next = nextFirst;
-            first = nextFirst;
-            second = nextSecond;
+            second->next = tmp1;
+            first = tmp1;
+            second = tmp2;
         }
-        // vector<int>v;
-        // ListNode *temp=head;
-        // while(temp!=NULL){
-        //     int y=temp->val;
-        //     v.push_back(y);
-        //     temp=temp->next;
-        // }
-        //  int n = v.size();
-        // int i=0, j=n-1;
-        // temp = head;
-        // while (i < j) {
-        //     temp->val = v[i];
-        //     temp=temp->next;
-        //     i++;
-        //     temp->val=v[j];
-        //     temp=temp->next;
-        //     j--;
-        // }
-        // if (i==j) {
-        //     temp->val=v[i];
-        // }
     }
 };
