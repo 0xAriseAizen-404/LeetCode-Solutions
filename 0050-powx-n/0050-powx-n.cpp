9dbf1dd855abfbf6c9333438f456ba4f692526e5
Mahesh Avvaru <maheshavvaru2004@gmail.com>
2024-10-02 13:54:27 +0530

    Time: 4 ms (22.59%), Space: 8.2 MB (73.35%) - LeetHub

--- 0050-powx-n/0050-powx-n.cpp
+++ 0050-powx-n/0050-powx-n.cpp
@@ -1,17 +1,31 @@
-
 class Solution {
 public:
-    double myPow(double x, long long n) {
-        if(n==0 || x==1)
-            return 1;
-        if(x==0)
-            return 0;
-        if(n<0)
-            return myPow(1/x,abs(n));
-
-        return (n%2==0 ? myPow(x*x,n/2) : x*myPow(x*x,n/2));
+    double solve(double x, long n){
+        if(n == 0) return 1 ; 
+        if(n < 0 ) return solve(1/x , -n);
+        if(n%2 ==0 )  return solve(x*x , n/2);
+        
+        return x*solve(x*x,(n-1)/2);   
+    }
+    double myPow(double x, int n) {
+        return solve(x,(long long)n);
     }
 };
+
+
+// class Solution {
+// public:
+//     double myPow(double x, long long n) {
+//         if(n==0 || x==1)
+//             return 1;
+//         if(x==0)
+//             return 0;
+//         if(n<0)
+//             return myPow(1/x,abs(n));
+
+//         return (n%2==0 ? myPow(x*x,n/2) : x*myPow(x*x,n/2));
+//     }
+// };
 
 // class Solution {
 // public:
