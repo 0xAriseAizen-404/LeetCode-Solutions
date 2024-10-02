b7659b1d00931881ddc61ebd22fb232b4455c629
Mahesh Avvaru <maheshavvaru2004@gmail.com>
2024-10-02 22:30:47 +0530

    Time: 23 ms (56.2%), Space: 26.9 MB (30.37%) - LeetHub

--- 0503-next-greater-element-ii/0503-next-greater-element-ii.cpp
+++ 0503-next-greater-element-ii/0503-next-greater-element-ii.cpp
@@ -1,16 +1,33 @@
 class Solution {
 public:
     vector<int> nextGreaterElements(vector<int>& nums) {
-        vector<int> newNums(nums);
-        newNums.insert(newNums.end(), nums.begin(), nums.end());
-        vector<int> res;
+        int n = nums.size();
+        vector<int> res(n, -1);
         stack<int> st;
-        for (int i=newNums.size()-1; i>=0; --i) {
-            while (!st.empty() && newNums[i] >= st.top()) st.pop();
-            if (i < nums.size()) res.push_back(st.empty() ? -1 : st.top());
-            st.push(newNums[i]);
+        for (int i = 2 * n - 1; i >= 0; --i) {
+            while (!st.empty() && nums[i % n] >= st.top()) st.pop();
+            if (i < n) res[i] = st.empty() ? -1 : st.top();
+            st.push(nums[i % n]);
         }
-        reverse(res.begin(), res.end());
+        
         return res;
     }
 };
+
+
+// class Solution {
+// public:
+//     vector<int> nextGreaterElements(vector<int>& nums) {
+//         vector<int> newNums(nums);
+//         newNums.insert(newNums.end(), nums.begin(), nums.end());
+//         vector<int> res;
+//         stack<int> st;
+//         for (int i=newNums.size()-1; i>=0; --i) {
+//             while (!st.empty() && newNums[i] >= st.top()) st.pop();
+//             if (i < nums.size()) res.push_back(st.empty() ? -1 : st.top());
+//             st.push(newNums[i]);
+//         }
+//         reverse(res.begin(), res.end());
+//         return res;
+//     }
+// };
