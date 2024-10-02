9ce6ef094ff959840bb57c21353a6973cd78c5ff
Mahesh Avvaru <maheshavvaru2004@gmail.com>
2024-10-02 23:50:29 +0530

    Time: 10 ms (84.05%), Space: 14.4 MB (55.78%) - LeetHub

--- 2227-sum-of-subarray-ranges/2227-sum-of-subarray-ranges.cpp
+++ 2227-sum-of-subarray-ranges/2227-sum-of-subarray-ranges.cpp
@@ -1,70 +1,51 @@
 class Solution {
 public:
-    long long sumSubArrayMaxs(vector<int> &nums) {
-        int n = nums.size();
-        vector<int> left(n, -1);
-        vector<int> right(n, n);
-        stack<int> st;
-
-        // prev-greater-element
-        for (int i = 0; i < n; ++i) {
-            while (!st.empty() && nums[st.top()] <= nums[i])
-                st.pop();
-            left[i] = st.empty() ? -1 : st.top();
-            st.push(i);
-        }
-
-        while (!st.empty()) st.pop();
-
-        // next-greater-element
-        for (int i = n - 1; i >= 0; --i) {
-            while (!st.empty() && nums[i] > nums[st.top()]) 
-                st.pop();
-            right[i] = st.empty() ? n : st.top();
-            st.push(i);
-        }
-
-        long long MOD = 1e9 + 7;
-        long long answer = 0;
-        for (int i = 0; i < n; ++i) {
-            answer = (answer + ((long long)(i - left[i]) * (right[i] - i) * nums[i]));
-        }
-        return answer;
-    }
-
-    long long sumSubArrayMins(vector<int> &nums) {
-        int n = nums.size();
-        vector<int> left(n, -1);
-        vector<int> right(n, n);
-        stack<int> st;
-
-        // prev-smaller-element
-        for (int i = 0; i < n; ++i) {
-            while (!st.empty() && nums[st.top()] >= nums[i])
-                st.pop();
-            left[i] = st.empty() ? -1 : st.top();
-            st.push(i);
-        }
-
-        while (!st.empty()) st.pop();
-
-        // next-smaller-element
-        for (int i = n - 1; i >= 0; --i) {
-            while (!st.empty() && nums[i] < nums[st.top()])
-                st.pop();
-            right[i] = st.empty() ? n : st.top();
-            st.push(i);
-        }
-
-        long long MOD = 1e9 + 7;
-        long long answer = 0;
-        for (int i = 0; i < n; ++i) {
-            answer = (answer + ((long long)(i - left[i]) * (right[i] - i) * nums[i]));
-        }
-        return answer;
-    }
-
     long long subArrayRanges(vector<int>& nums) {
-        return sumSubArrayMaxs(nums) - sumSubArrayMins(nums);
+        int n = nums.size();
+
+        vector<int> leftMin(n, 0), rightMin(n, 0);
+        vector<int> leftMax(n, 0), rightMax(n, 0);
+        stack<int> minStack;
+        stack<int> maxStack;
+
+        for(int i = 0; i < n; i++) {
+            while(!minStack.empty() && nums[minStack.top()] > nums[i])
+                minStack.pop();
+
+            leftMin[i] = minStack.empty() ? -1 : minStack.top();
+            minStack.push(i);
+
+            while(!maxStack.empty() && nums[maxStack.top()] < nums[i])
+                maxStack.pop();
+
+            leftMax[i] = maxStack.empty() ? -1: maxStack.top();
+            maxStack.push(i);
+        }
+
+        minStack = stack<int>();
+        maxStack = stack<int>();
+
+        for(int i = n-1; i >= 0; i--) {
+            while(!minStack.empty() && nums[minStack.top()] >= nums[i])
+                minStack.pop();
+
+            rightMin[i] = minStack.empty() ? n : minStack.top();
+            minStack.push(i);
+
+            while(!maxStack.empty() && nums[maxStack.top()] <= nums[i])
+                maxStack.pop();
+
+            rightMax[i] = maxStack.empty() ? n: maxStack.top();
+            maxStack.push(i);
+        }
+
+        long long sumOfMaxes = 0, sumOfMins = 0;
+
+        for(int i = 0; i < n; i++) {
+            sumOfMaxes += (long long)nums[i]*(i-leftMax[i])*(rightMax[i]-i);
+            sumOfMins += (long long)nums[i]*(i-leftMin[i])*(rightMin[i]-i);
+        }
+
+        return sumOfMaxes - sumOfMins;
     }
 };
