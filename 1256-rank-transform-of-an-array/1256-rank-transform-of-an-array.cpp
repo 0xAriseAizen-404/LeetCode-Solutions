7271836a6f01bfdbf16c068bf6f28ac07d582ccb
Mahesh Avvaru <maheshavvaru2004@gmail.com>
2024-10-02 08:51:30 +0530

    Time: 62 ms (78.39%), Space: 43.1 MB (59.66%) - LeetHub

--- 1256-rank-transform-of-an-array/1256-rank-transform-of-an-array.cpp
+++ 1256-rank-transform-of-an-array/1256-rank-transform-of-an-array.cpp
@@ -1,20 +1,36 @@
 class Solution {
 public:
     vector<int> arrayRankTransform(vector<int>& arr) {
-        priority_queue<int, vector<int>, greater<int>> pq;
-        for (const auto &x: arr) pq.push(x);
+
+        vector<int> sortedArr = arr;
+        sort(sortedArr.begin(), sortedArr.end());
         unordered_map<int, int> umap;
         int rank = 1;
-        while (!pq.empty()) {
-            if (umap.find(pq.top()) == umap.end()) {
-                umap[pq.top()] = rank++;
+        for (const auto &x : sortedArr) {
+            if (umap.find(x) == umap.end()) {
+                umap[x] = rank++;
             }
-            pq.pop();
         }
-        vector<int> res(arr.size(), 1);
-        for (int i=0; i<arr.size(); ++i) {
+        vector<int> res(arr.size());
+        for (int i = 0; i < arr.size(); ++i) {
             res[i] = umap[arr[i]];
         }
         return res;
+
+        // priority_queue<int, vector<int>, greater<int>> pq;
+        // for (const auto &x: arr) pq.push(x);
+        // unordered_map<int, int> umap;
+        // int rank = 1;
+        // while (!pq.empty()) {
+        //     if (umap.find(pq.top()) == umap.end()) {
+        //         umap[pq.top()] = rank++;
+        //     }
+        //     pq.pop();
+        // }
+        // vector<int> res(arr.size(), 1);
+        // for (int i=0; i<arr.size(); ++i) {
+        //     res[i] = umap[arr[i]];
+        // }
+        // return res;
     }
 };
