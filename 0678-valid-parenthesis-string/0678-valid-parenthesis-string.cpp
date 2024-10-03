8d9d5c40b8d8d0f761021c29488cf500227feda0
Mahesh Avvaru <maheshavvaru2004@gmail.com>
2024-10-03 15:30:31 +0530

    Time: 3 ms (51.77%), Space: 7.6 MB (62.08%) - LeetHub

--- 0678-valid-parenthesis-string/0678-valid-parenthesis-string.cpp
+++ 0678-valid-parenthesis-string/0678-valid-parenthesis-string.cpp
@@ -1,16 +1,22 @@
 class Solution {
 public:
     bool checkValidString(string s) {
-        int length = s.length() - 1;
-int openCount = 0, closedCount = 0;
-for (int i = 0; i <= length; i++)
-{
-	if (s[i] == '*' || s[i] == '(') openCount++;
-	else openCount--;
-	if (s[length - i] == '*' || s[length - i] == ')') closedCount++;
-	else closedCount--;
-	if (openCount < 0 || closedCount < 0) return false;
-}
-return true;
+        int maxOpen = 0;
+        int minOpen = 0;
+        for (const auto &x: s) {
+            if (x == '(') {
+                maxOpen++;
+                minOpen++;
+            } else if (x == ')') {
+                maxOpen--;
+                minOpen--;
+            } else {
+                maxOpen++;
+                minOpen--;
+            }
+            if (maxOpen < 0) return false;
+            minOpen = max(minOpen, 0);
+        }
+        return minOpen == 0;
     }
 };
