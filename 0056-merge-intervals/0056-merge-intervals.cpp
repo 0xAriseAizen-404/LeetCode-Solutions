d8cb331ffa56e4cf9f795719a59d677b5f741d3d
Mahesh Avvaru <maheshavvaru2004@gmail.com>
2024-10-03 08:55:03 +0530

    Time: 23 ms (61.73%), Space: 24 MB (15.57%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.cpp
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -2,12 +2,12 @@ class Solution {
 public:
     vector<vector<int>> merge(vector<vector<int>>& intervals) {
         sort(intervals.begin(), intervals.end(), [](const vector<int> &a, const vector<int> &b) {
-            return a[0] <= b[0];
+            return a[0] == b[0] ? a[1] < b[1] : a[0] < b[0];
         });
         vector<vector<int>> res;
-        for (vector<int> coming: intervals) {
+        for (vector<int> coming : intervals) {
             if (!res.empty() && coming[0] <= res.back()[1]) {
-                res.back()[1] = max(res.back()[1] , coming[1]);
+                res.back()[1] = max(res.back()[1], coming[1]);
             } else {
                 res.push_back(coming);
             }
