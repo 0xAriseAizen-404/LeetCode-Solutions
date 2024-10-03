3e0306359c2437a868617f57beb467dd2c01f319
Mahesh Avvaru <maheshavvaru2004@gmail.com>
2024-10-03 10:51:45 +0530

    Time: 13 ms (36.65%), Space: 10.1 MB (89.44%) - LeetHub

--- 0402-remove-k-digits/0402-remove-k-digits.cpp
+++ 0402-remove-k-digits/0402-remove-k-digits.cpp
@@ -1,27 +1,45 @@
 class Solution {
 public:
     string removeKdigits(string num, int k) {
-        string res = "";
-        stack<int> st;
-        for (int i = 0; i < num.size(); ++i) {
-            while (!st.empty() && k > 0 && num[i] < num[st.top()]) {
+        string ans="";
+        for(char &c:num) {
+            while(ans.size() && ans.back()>c && k) {
+                ans.pop_back();
                 k--;
-                st.pop();
             }
-            st.push(i);
+            if(ans.size() || c!='0') ans.push_back(c);
         }
-        while (k > 0 && !st.empty()) {
-            st.pop();
-            k--;
+        while(ans.size() && k--) {
+            ans.pop_back();
         }
-        if (st.empty()) return "0";
-        while (!st.empty()) {
-            res += num[st.top()];
-            st.pop();
-        }
-        reverse(res.begin(), res.end());
-        while (!res.empty() && res[0] == '0') res.erase(res.begin());
-        if (res.empty()) return "0";
-        return res;
+        return (ans=="")?"0":ans;
     }
 };
+
+// class Solution {
+// public:
+//     string removeKdigits(string num, int k) {
+//         string res = "";
+//         stack<int> st;
+//         for (int i = 0; i < num.size(); ++i) {
+//             while (!st.empty() && k > 0 && num[i] < num[st.top()]) {
+//                 k--;
+//                 st.pop();
+//             }
+//             st.push(i);
+//         }
+//         while (k > 0 && !st.empty()) {
+//             st.pop();
+//             k--;
+//         }
+//         if (st.empty()) return "0";
+//         while (!st.empty()) {
+//             res += num[st.top()];
+//             st.pop();
+//         }
+//         reverse(res.begin(), res.end());
+//         while (!res.empty() && res[0] == '0') res.erase(res.begin());
+//         if (res.empty()) return "0";
+//         return res;
+//     }
+// };
