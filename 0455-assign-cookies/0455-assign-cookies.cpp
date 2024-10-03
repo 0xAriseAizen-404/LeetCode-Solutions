6afd98f7817871ea1b21ecdc17bb1539dd3eb3eb
Mahesh Avvaru <maheshavvaru2004@gmail.com>
2024-10-03 15:12:45 +0530

    Time: 26 ms (67.93%), Space: 44.7 MB (45.06%) - LeetHub

--- 0455-assign-cookies/0455-assign-cookies.cpp
+++ 0455-assign-cookies/0455-assign-cookies.cpp
@@ -1,6 +1,8 @@
 class Solution {
 public:
     int findContentChildren(vector<int>& g, vector<int>& s) {
+        ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+        if (s.size() == 0) return 0;
         sort(g.begin(), g.end());
         sort(s.begin(), s.end());
         int childrenInd = 0;
