b14c059b47bf63503f8c91abee27884be3dc2cb2
Mahesh Avvaru <maheshavvaru2004@gmail.com>
2024-10-04 16:33:01 +0530

    Time: 9 ms (84.88%), Space: 13.1 MB (70.06%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -1,45 +1,45 @@
 class Solution {
+private:
+    int binarySearch(vector<int> &dp, int target) {
+        int low = 0;
+        int high = dp.size() - 1;
+        while (low < high) {
+            int mid = (low + high) >> 1;
+            if (dp[mid] == target) return mid;
+            else if (dp[mid] < target) low = mid + 1;
+            else high = mid;
+        }
+        return low;
+    }
 public:
     int lengthOfLIS(vector<int>& nums) {
-        int n = nums.size();
-        int maxLen = 1;
-        vector<int> dp(n, 1);
-        for (int i=1; i<n; ++i) {
-            for (int j=0; j<i; ++j) {
-                if (nums[i] > nums[j]) {
-                    dp[i] = max(dp[i], 1 + dp[j]);
-                    maxLen = max(maxLen, dp[i]);
-                }
+        vector<int> dp;
+        dp.push_back(nums[0]);
+        for (int i=1; i<nums.size(); ++i) {
+            if (dp.back() < nums[i]) dp.push_back(nums[i]);
+            else {
+                int indToInsert = binarySearch(dp, nums[i]);
+                dp[indToInsert] = nums[i];
             }
         }
-        return maxLen;
+        return dp.size();
     }
 };
 
 // class Solution {
-// private:
-//     int binarySearch(vector<int> &dp, int target) {
-//         int low = 0;
-//         int high = dp.size() - 1;
-//         while (low < high) {
-//             int mid = (low + high) >> 1;
-//             if (dp[mid] == target) return mid;
-//             else if (dp[mid] < target) low = mid + 1;
-//             else high = mid;
-//         }
-//         return low;
-//     }
 // public:
 //     int lengthOfLIS(vector<int>& nums) {
-//         vector<int> dp;
-//         dp.push_back(nums[0]);
-//         for (int i=1; i<nums.size(); ++i) {
-//             if (dp.back() < nums[i]) dp.push_back(nums[i]);
-//             else {
-//                 int indToInsert = binarySearch(dp, nums[i]);
-//                 dp[indToInsert] = nums[i];
+//         int n = nums.size();
+//         int maxLen = 1;
+//         vector<int> dp(n, 1);
+//         for (int i=1; i<n; ++i) {
+//             for (int j=0; j<i; ++j) {
+//                 if (nums[i] > nums[j]) {
+//                     dp[i] = max(dp[i], 1 + dp[j]);
+//                     maxLen = max(maxLen, dp[i]);
+//                 }
 //             }
 //         }
-//         return dp.size();
+//         return maxLen;
 //     }
 // };
