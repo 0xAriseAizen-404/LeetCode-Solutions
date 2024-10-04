6520836d0c1cc385e46f8696d1b4b101d4b918df
Mahesh Avvaru <maheshavvaru2004@gmail.com>
2024-10-04 09:59:10 +0530

    Time: 20 ms (85.92%), Space: 16.4 MB (74.23%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -1,12 +1,14 @@
 class Solution {
 public:
     int coinChange(vector<int>& coins, int amount) {
+        // (amount + 1) - as value is an invalid or impossible case look for return statement to understand why (amount + 1) used;
         vector<int> amt(amount + 1, amount + 1);
         amt[0] = 0;
         for (int val=1; val<=amount; ++val) {
             for (auto const &x: coins) {
-                if (val >= x)
+                if (x <= val)
                     amt[val] = min(amt[val], 1 + amt[val - x]);
+                    // here 1 + amt[...] represents already naa dhaggara oka coin undhi val kante thakkuva so idhi nen theesukunna one quantity inka naku yentha kaavali (val - x) in tabulation => 1 + amt[val - x];
             }
         }
         return amt[amount] < amount + 1 ? amt[amount] : -1;
