e8cf35de534d8e38c6785c53f250131fd064f1c6
Mahesh Avvaru <maheshavvaru2004@gmail.com>
2024-11-04 19:07:52 +0530

    Time: 6 ms (54.94%), Space: 42 MB (64.25%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cs
+++ 0242-valid-anagram/0242-valid-anagram.cs
@@ -1,14 +1,17 @@
 using System;
+using System.Collections.Generic;
+
 public class Solution {
     public bool IsAnagram(string s, string t) {
-        if (s.Length != t.Length) return false;
-
-        char[] sArray = s.ToCharArray();
-        char[] tArray = t.ToCharArray();
-
-        Array.Sort(sArray);
-        Array.Sort(tArray);
-
-        return new string(sArray).Equals(new string(tArray));
+        var freq = new Dictionary<char, int>();
+        foreach (char x in s) {
+            if (freq.ContainsKey(x)) freq[x]++;
+            else freq[x] = 1;
+        }
+        foreach (char x in t) {
+            if (!freq.ContainsKey(x)) return false;
+            if (--freq[x] == 0) freq.Remove(x);
+        }
+        return freq.Count == 0;
     }
 }
