13215a5ba2a70a5c245523adf190846648e56972
Mahesh Avvaru <maheshavvaru2004@gmail.com>
2024-11-04 19:26:51 +0530

    Time: 4 ms (97.21%), Space: 50.5 MB (63.44%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cs
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cs
@@ -1,11 +1,31 @@
-public class Solution 
+public class Solution
 {
-    public int[] TopKFrequent(int[] nums, int k) 
-    {
-        return nums.GroupBy(num => num)
-        .OrderByDescending(num => num.Count())
-        .Take(k)
-        .Select(c => c.Key)
-        .ToArray();
-    }
+	public int[] TopKFrequent(int[] nums, int k)
+	{
+		var dict = new Dictionary<int, int>();
+		for (int i = 0; i < nums.Length; i++)
+		{
+			var num = nums[i];
+			if(dict.ContainsKey(num))
+			{
+				dict[num]++;
+			}
+			else dict.Add(num, 1);
+		}
+		//return dict.OrderByDescending(x => x.Value).Take(k).Select(x => x.Key).ToArray(); O(n log n)
+		var buckets = new List<int>[nums.Length + 1];
+		foreach (var num in dict.Keys)
+		{
+			var freq = dict[num];
+			if(buckets[freq] == null) buckets[freq] = new List<int>();
+			buckets[freq].Add(num);
+		}
+		
+		var result = new List<int>();
+		for (int i = buckets.Length - 1; i >= 0 && result.Count < k; i--)
+		{
+			if(buckets[i] != null) result.AddRange(buckets[i]);
+		}
+		return result.ToArray();
+	}
 }
