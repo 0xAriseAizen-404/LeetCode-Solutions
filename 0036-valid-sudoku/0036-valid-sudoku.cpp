08573f392ddbb2411adb37c4c98e46c6e1536f1b
Mahesh Avvaru <maheshavvaru2004@gmail.com>
2024-11-04 19:35:19 +0530

    Time: 22 ms (5.25%), Space: 26.4 MB (9.52%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.cpp
+++ 0036-valid-sudoku/0036-valid-sudoku.cpp
@@ -1,22 +1,20 @@
 class Solution {
 public:
     bool isValidSudoku(vector<vector<char>>& board) {
-        unordered_map<string, int> map;
-        for(int i=0; i<9; ++i) {
-            for(int j=0; j<9; ++j) {
-                char currVal = board[i][j];
-                if(currVal != '.') {
-                    string row_key = to_string(i) + "row" + currVal;
-                    string col_key = to_string(j) + "col" + currVal;
-                    string grid_key = to_string(i/3) + "-" + to_string(j/3) + "grid" + currVal;
-                    if(map[row_key] != 0 || map[col_key] != 0 || map[grid_key] != 0) return false;
-                    else 
-                        map[row_key]++;
-                        map[col_key]++;
-                        map[grid_key]++;
-                    }
+        unordered_map<int, unordered_map<char, int>> map;
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                char cell = board[i][j];
+                if (cell != '.') {
+                    if (map[i][cell]++ > 0)
+                        return false;
+                    if (map[j + 9][cell]++ > 0)
+                        return false;
+                    if (map[(i / 3) * 3 + (j / 3) + 18][cell]++)
+                        return false;
                 }
             }
+        }
         return true;
     }
 };
