801ccdf4750dc5b4103ccec217fc616a092a812d
Mahesh Avvaru <maheshavvaru2004@gmail.com>
2024-11-04 19:02:30 +0530

    Time: 6 ms (54.94%), Space: 42.6 MB (28.3%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.cs
@@ -0,0 +1,14 @@
+using System;
+public class Solution {
+    public bool IsAnagram(string s, string t) {
+        if (s.Length != t.Length) return false;
+
+        char[] sArray = s.ToCharArray();
+        char[] tArray = t.ToCharArray();
+
+        Array.Sort(sArray);
+        Array.Sort(tArray);
+
+        return new string(sArray).Equals(new string(tArray));
+    }
+}
