502adbf952b78728b60e01f899f525dee90cdd61
Mahesh Avvaru <maheshavvaru2004@gmail.com>
2025-03-17 15:34:24 +0530

    Time: 0 ms (100%), Space: 17.1 MB (66.52%) - LeetHub

--- 2308-divide-array-into-equal-pairs/2308-divide-array-into-equal-pairs.cpp
+++ 2308-divide-array-into-equal-pairs/2308-divide-array-into-equal-pairs.cpp
@@ -1,15 +1,27 @@
-#include <vector>
-#include <unordered_map>
-using namespace std;
+// class Solution {
+// public:
+//     bool divideArray(vector<int>& nums) {
+//         unordered_map<int, int> freq;
+//         for (int num : nums) freq[num]++;
+//         for (auto &entry : freq) {
+//             if (entry.second & 1) return false;
+//         }
+//         return true;
+//     }
+// };
+
 
 class Solution {
 public:
     bool divideArray(vector<int>& nums) {
-        unordered_map<int, int> freq;
-        for (int num : nums) freq[num]++;
-        for (auto &entry : freq) {
-            if (entry.second & 1) return false;
+        int freq[501] = {0};
+        for (int num : nums) {
+            freq[num] ^= 1; // Toggle between 0 and 1
         }
+        for (int i = 1; i <= 500; i++) {
+            if (freq[i] == 1) return false; // If any bit is set, we have an odd occurrence
+        }
+        
         return true;
     }
 };
