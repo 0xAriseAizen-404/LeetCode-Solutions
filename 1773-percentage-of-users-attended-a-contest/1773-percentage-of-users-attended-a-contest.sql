40c981e3250d8f82ff8f475d52f9d75c9495cf30
Mahesh Avvaru <maheshavvaru2004@gmail.com>
2025-08-23 22:24:55 +0530

    Time: 949 ms (64.19%), Space: 0B (100%) - LeetHub

--- 1773-percentage-of-users-attended-a-contest/1773-percentage-of-users-attended-a-contest.sql
+++ 1773-percentage-of-users-attended-a-contest/1773-percentage-of-users-attended-a-contest.sql
@@ -1,13 +1,15 @@
-# Write your MySQL query statement below
-SELECT
-    r.contest_id,
-    ROUND(COUNT(*) * 100.0 / (SELECT COUNT(*) FROM users), 2) AS percentage
-FROM
-    users u
-INNER JOIN
-    register r ON u.user_id = r.user_id
-GROUP BY
-    r.contest_id
-ORDER BY
-    percentage DESC,
-    r.contest_id ASC;
+-- My Version
+SELECT 
+    contest_id, 
+    ROUND(COUNT(*) * 100 / (SELECT COUNT(*) FROM users), 2) AS percentage
+FROM register
+GROUP BY contest_id
+ORDER BY percentage DESC, contest_id ASC;
+
+-- ChatGPT Version
+-- SELECT 
+--     r.contest_id, 
+--     ROUND(COUNT(DISTINCT r.user_id) * 100.0 / (SELECT COUNT(*) FROM users), 2) AS percentage
+-- FROM register AS r
+-- GROUP BY r.contest_id
+-- ORDER BY percentage DESC, r.contest_id ASC;
