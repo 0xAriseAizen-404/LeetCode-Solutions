eaca6b97210b29c82b40dbd0f6c4841507e51f55
Mahesh Avvaru <maheshavvaru2004@gmail.com>
2026-06-24 22:22:48 +0530

    Time: 0 ms (100%), Space: 46.2 MB (63.82%) - LeetHub

--- 1538-maximum-points-you-can-obtain-from-cards/1538-maximum-points-you-can-obtain-from-cards.cpp
+++ 1538-maximum-points-you-can-obtain-from-cards/1538-maximum-points-you-can-obtain-from-cards.cpp
@@ -1,19 +1,18 @@
-// own logic
-
 class Solution {
 public:
     int maxScore(vector<int>& cardPoints, int k) {
         ios_base::sync_with_stdio(false); cin.tie(NULL);
-        int n = cardPoints.size();
-        int *beginPresum = new int[k+1]();
-        int *endPresum = new int[k+1]();
-        for (int i=0; i<k; ++i) {
-            beginPresum[i+1] = beginPresum[i] + cardPoints[i];
-            endPresum[i+1] = endPresum[i] + cardPoints[n-i-1];
+        int sz = cardPoints.size();
+        int leftSum = 0;
+        for (int i=0; i<k; ++i) leftSum += cardPoints[i];
+        int maxSum = leftSum;
+        if (k == cardPoints.size()) return leftSum;
+        int rightSum = 0;
+        for (int i=1; i<=k; ++i) {
+            leftSum -= cardPoints[k-i];
+            rightSum += cardPoints[sz-i];
+            maxSum = max(maxSum, leftSum + rightSum);
         }
-        // for (int i=0; i<k+1; ++i) cout << beginPresum[i] << " " << endPresum[i] << endl;
-        int max_score = INT_MIN;
-        for (int i=0; i<k+1; ++i) max_score = max(max_score, (beginPresum[i] + endPresum[k-i]));
-        return max_score;
+        return maxSum;
     }
 };
