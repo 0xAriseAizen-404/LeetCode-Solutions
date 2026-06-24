b977ee043fb29cb7358147f322ed5cd9efa22312
Mahesh Avvaru <maheshavvaru2004@gmail.com>
2026-06-24 21:54:23 +0530

    Time: 4 ms (7.06%), Space: 22.9 MB (83.07%) - LeetHub

--- 0110-balanced-binary-tree/0110-balanced-binary-tree.cpp
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.cpp
@@ -11,17 +11,52 @@
  */
 class Solution {
 private:
-    int myHelper(TreeNode* node, bool &isValid) {
+    int getHeight(TreeNode *node) {
         if (!node) return 0;
-        int lH = myHelper(node->left, isValid);
-        int rH = myHelper(node->right, isValid);
-        if (abs(lH - rH) > 1) isValid = 0;
-        return 1 + max(lH, rH);
+        int leftH = getHeight(node->left);
+        int rightH = getHeight(node->right);
+        if (leftH == -1 || rightH == -1 || abs(leftH - rightH) > 1) return -1;
+        else return max(leftH, rightH) + 1;
     }
 public:
     bool isBalanced(TreeNode* root) {
-        bool isValid = 1;
-        myHelper(root, isValid);
-        return isValid;
+        return getHeight(root) != -1;
     }
 };
+// TC: O(n)
+// SC: O(h)  // O(log n) balanced, O(n) skewed
+
+// class Solution {
+// private:
+//     int getHeightCheck(TreeNode *node, bool &isValid) {
+//         if (!node) return 0;
+//         int leftHeight = getHeightCheck(node->left, isValid);
+//         int rightHeight = getHeightCheck(node->right, isValid);
+//         if (abs(leftHeight - rightHeight) > 1) isValid = false;
+//         return max(leftHeight, rightHeight) + 1;
+//     }
+// public:
+//     bool isBalanced(TreeNode* root) {
+//         bool isValid = true;
+//         getHeightCheck(root, isValid);
+//         return isValid;
+//     }
+// };
+// TC: O(n)
+// SC: O(h)  // O(log n) balanced, O(n) skewed
+
+// class Solution {
+// private:
+//     int getHeight(TreeNode *node) {
+//         if (!node) return 0;
+//         return max(getHeight(node->left), getHeight(node->right)) + 1;
+//     }
+// public:
+//     bool isBalanced(TreeNode* root) {
+//         if (!root) return true;
+//         if (abs(getHeight(root->left) - getHeight(root->right)) > 1) return false;
+//         return isBalanced(root->left) && isBalanced(root->right);
+//     }
+// };
+// TC: O(n²) worst case, O(n log n) balanced
+// SC: O(h)  // O(log n) balanced, O(n) skewed
