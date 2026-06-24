319ce4afb4fe2a83b11b8007f5ca94c257f31fe9
Mahesh Avvaru <maheshavvaru2004@gmail.com>
2026-06-24 22:27:15 +0530

    Time: 80 ms (65.47%), Space: 55.4 MB (73.39%) - LeetHub

--- 1034-subarrays-with-k-different-integers/1034-subarrays-with-k-different-integers.cpp
+++ 1034-subarrays-with-k-different-integers/1034-subarrays-with-k-different-integers.cpp
@@ -1,47 +1,46 @@
+// Optimal Solution
 class Solution {
 private:
-    int myHelper(vector<int>& nums, int k) {
-        unordered_map<int, int> freq;
+    int helper(vector<int> &nums, int k) {
         int left = 0;
-        int right = 0;
-        int count = 0;
-        while (right < nums.size()) {
-            freq[nums[right]]++;
-            while (freq.size() > k) {
-                if (--freq[nums[left]] == 0) {
-                    freq.erase(nums[left]);
-                }
+        int cnt = 0;
+        unordered_map<int, int> umap;
+        for (int right = 0; right < nums.size(); ++right) {
+            ++umap[nums[right]];
+            while (umap.size() > k) {
+                --umap[nums[left]];
+                if (umap[nums[left]] == 0) umap.erase(nums[left]);
                 left++;
             }
-            count += (right - left + 1);
-            right++;
+            cnt += (right - left + 1);
         }
-        return count;
+        return cnt;
     }
 public:
     int subarraysWithKDistinct(vector<int>& nums, int k) {
-        return myHelper(nums, k) - myHelper(nums, k - 1);
+        if (k == 1) return helper(nums, k);
+        return helper(nums, k) - helper(nums, k-1);
     }
 };
+// TC: O(2N)
+// SC: O(K)
 
-
+// BruteForce Solution
 // class Solution {
-// private:
-//     int myHelper(vector<int>& nums, int k) {
-//         unordered_set<int> sset;
-//         int left = 0;
-//         int right = 0;
-//         int count = 0;
-//         while (right < nums.size()) {
-//             sset.insert(nums[right]);
-//             while (sset.size() > k) sset.erase(nums[left++]);
-//             count += (right - left + 1);
-//             right++;
-//         }
-//         return count;
-//     }
 // public:
 //     int subarraysWithKDistinct(vector<int>& nums, int k) {
-//         return myHelper(nums, k) - myHelper(nums, k - 1);
+//         int sz = nums.size();
+//         int cnt = 0;
+//         for (int i = 0; i < sz; i++) {
+//             unordered_map<int, int> umap;
+//             for (int j = i; j < sz; j++) {
+//                 umap[nums[j]]++;
+//                 if (umap.size() == k) cnt++;
+//                 else if (umap.size() > k) break;
+//             }
+//         }
+//         return cnt;
 //     }
 // };
+// TC: O(N^2)
+// SC: O(K)
