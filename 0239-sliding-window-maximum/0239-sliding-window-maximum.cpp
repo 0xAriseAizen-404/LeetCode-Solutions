4d85fcf0a35e61ab8851eec51f478e4253224bd0
Mahesh Avvaru <maheshavvaru2004@gmail.com>
2026-06-29 20:22:44 +0530

    Time: 22 ms (61.36%), Space: 139 MB (89.69%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
@@ -1,44 +1,28 @@
 class Solution {
 public:
     vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+        int sz = nums.size();
         vector<int> res;
         deque<int> dq;
-        
-        for (int i = 0; i < nums.size(); ++i) {
-            // Remove index of element not within the sliding window
-            if (!dq.empty() && dq.front() == i - k) {
-                dq.pop_front();
-            }
-            while (!dq.empty() && nums[dq.back()] <= nums[i]) {
-                dq.pop_back();
-            }
+        for (int i=0; i<sz; ++i) {
+            if (!dq.empty() && dq.front() == i-k) dq.pop_front();
+            while (!dq.empty() && nums[dq.back()] <= nums[i]) dq.pop_back();
             dq.push_back(i);
-            if (i >= k - 1) {
-                res.push_back(nums[dq.front()]);
-            }
+            if (i >= k-1) res.push_back(nums[dq.front()]);
         }
-        
-        return res;
+        return res; 
     }
 };
 
-
+// TLE
 // class Solution {
 // public:
 //     vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+//         int sz = nums.size();
 //         vector<int> res;
-//         deque<int> dq;
-//         for (int i=0; i<k; ++i) {
-//             while (!dq.empty() && dq.front() < nums[i]) dq.pop_front();
-//             dq.push_front(nums[i]);
-//         }
-//         res.push_back(dq.front());
-//         int left = 0;
-//         for (int i=k; i<nums.size(); ++i) {
-//             dq.erase(nums[left]);
-//             while (dq.front() < nums[i]) dq.pop_front();
-//             dq.push_front(nums[i]);
-//             res.push_back(dq.front());
+//         for (int i=0; i<=sz-k; ++i) {
+//             int max_ele = *max_element(nums.begin()+i, nums.begin()+i+k);
+//             res.push_back(max_ele);
 //         }
 //         return res;
 //     }
