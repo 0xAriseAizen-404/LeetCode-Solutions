d98d8bd614f79e09d8639c75f277a1f0794dc7bc
Mahesh Avvaru <maheshavvaru2004@gmail.com>
2026-06-29 20:09:38 +0530

    Time: 3 ms (28.2%), Space: 22 MB (59.7%) - LeetHub

--- 0735-asteroid-collision/0735-asteroid-collision.cpp
+++ 0735-asteroid-collision/0735-asteroid-collision.cpp
@@ -1,28 +1,55 @@
 class Solution {
 public:
     vector<int> asteroidCollision(vector<int>& asteroids) {
+        int sz = asteroids.size();
+        vector<int> ans;
         stack<int> st;
-        for (auto &x: asteroids) {
-            bool canPlace = true;
-            while (!st.empty() && st.top() > 0 && x < 0) {
-                if (st.top() < abs(x)) st.pop();
-                else if (st.top() > abs(x)) {
-                    canPlace = false;
-                    break;
-                } else {
-                    st.pop();
-                    canPlace = false;
-                    break;
+        for (int &ast: asteroids) {
+            if (ast > 0) st.push(ast);
+            else {
+                bool isDestroyed = false;
+                while (!st.empty()) {
+                    if (st.top() < abs(ast)) st.pop();
+                    else if (st.top() == abs(ast)) {
+                        st.pop();
+                        isDestroyed = true;
+                        break;
+                    } else {
+                        isDestroyed = true;
+                        break;
+                    }
                 }
+                if (!isDestroyed) ans.push_back(ast); // negative one
             }
-            if (canPlace) st.push(x);
         }
-        vector<int> res(st.size(), 0);
-        int ind = res.size() - 1;
-        while (!st.empty()) {
-            res[ind--] = st.top();
+        sz = ans.size();
+        ans.resize(ans.size() + st.size());
+        for (int i=st.size()-1; i>=0; --i) { // all the survived positive ones
+            ans[sz + i] = st.top();
             st.pop();
         }
-        return res;
+        return ans;
     }
 };
+
+// This is Wrong
+// class Solution {
+// public:
+//     vector<int> asteroidCollision(vector<int>& asteroids) {
+//         int sz = asteroids.size();
+//         stack<int> st;
+//         for (int i=0; i<sz; ++i) {
+//             while (!st.empty() && (st.top()>0 && asteroids[i]<0)) {
+//                 if (abs(st.top()) < abs(asteroids[i])) st.pop();
+//                 else continue;
+//              }
+//             st.push(asteroids[i]);
+//         }
+//         vector<int> ans(st.size(), 0);
+//         for (int i=st.size()-1; i>=0; --i) {
+//             ans[i] = st.top();
+//             st.pop();
+//         }
+//         return ans;
+//     }
+// };
