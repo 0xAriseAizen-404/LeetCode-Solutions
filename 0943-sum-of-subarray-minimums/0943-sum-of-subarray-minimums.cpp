f4b379ce1d37497d6b610a9e8f8c27f135e2179c
Mahesh Avvaru <maheshavvaru2004@gmail.com>
2026-06-29 19:58:56 +0530

    Time: 59 ms (44.67%), Space: 193.2 MB (43.55%) - LeetHub

--- 0943-sum-of-subarray-minimums/0943-sum-of-subarray-minimums.cpp
+++ 0943-sum-of-subarray-minimums/0943-sum-of-subarray-minimums.cpp
@@ -1,41 +1,37 @@
 class Solution {
 public:
     int sumSubarrayMins(vector<int>& arr) {
-        int length = arr.size();
-        vector<int> left(length, -1);
-        vector<int> right(length, length);
-
-        stack<int> st;
-
-        for (int i = 0; i < length; ++i) {
-            while (!st.empty() && arr[st.top()] >= arr[i]) {
-                st.pop();
-            }
-            if (!st.empty()) {
-                left[i] = st.top();
-            }
+        int sz = arr.size();
+        vector<int> nse(sz, sz);
+        vector<int> pse(sz, -1);
+        stack<int> st; // Minimum Increasing Stack;
+        // NSE
+        for (int i=sz-1; i>=0; --i) {
+            while (!st.empty() && arr[st.top()] >= arr[i]) st.pop();
+            nse[i] = st.empty() ? sz : st.top();
             st.push(i);
         }
-
         while (!st.empty()) st.pop();
-
-        for (int i = length - 1; i >= 0; --i) {
-            while (!st.empty() && arr[st.top()] > arr[i]) {
-                st.pop();
-            }
-            if (!st.empty()) {
-                right[i] = st.top();
-            }
+        // PSE
+        for (int i=0; i<sz; ++i) {
+            while (!st.empty() && arr[st.top()] > arr[i]) st.pop();
+            pse[i] = st.empty() ? -1 : st.top();
             st.push(i);
         }
+        // here we are counting the subarrays
+        // so normally, nse >= &&&& pse >=, both are same tie breaking way.
+        // but here in the counting subarray, we will get overlapping ambiguity i.e both numbers dominate same ranges, if we use same condition for both side, we need to use like nse >= && pse > (or) nse > && pse >=.
 
-        long mod = 1e9 + 7;
-        long long answer = 0;
+        // x GGSmall x small x x x small x x x GSmall x x x.
 
-        for (int i = 0; i < length; ++i) {
-            answer = (answer + (long long)(i - left[i]) * (right[i] - i) % mod * arr[i] % mod) % mod;
+        // here if first case, both small_1 will have range from GGSmall to GSmall &&&& small_2 will have range from GGSmall to GSmall.
+        // if in second case, small_1 will have range from GGSmall to GSmall &&&& small_2 will have range from small_1 to GSmall.
+        // see the difference between ranges.
+        int ans = 0;
+        int mod = 1e9+7;
+        for (int i=0; i<sz; ++i) {
+            ans = (ans + 1LL * (nse[i] - i) * (i - pse[i]) * arr[i] % mod) % mod;
         }
-
-        return (int) answer;
+        return ans;
     }
 };
