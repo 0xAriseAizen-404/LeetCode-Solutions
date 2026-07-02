5e0b45d8b2c0de7e6bd8ff01aaa430ec3a1de8a8
Mahesh Avvaru <maheshavvaru2004@gmail.com>
2026-07-02 23:47:27 +0530

    Time: 0 ms (100%), Space: 8.7 MB (43.15%) - LeetHub

--- 0050-powx-n/0050-powx-n.cpp
+++ 0050-powx-n/0050-powx-n.cpp
@@ -1,49 +1,44 @@
-class Solution {
-public:
-    double solve(double x, long n){
-        if(n == 0) return 1 ; 
-        if(n < 0 ) return solve(1/x , -n);
-        if(n%2 ==0 )  return solve(x*x , n/2);
-        
-        return x*solve(x*x,(n-1)/2);   
-    }
-    double myPow(double x, int n) {
-        return solve(x,(long long)n);
-    }
-};
-
-
-// class Solution {
-// public:
-//     double myPow(double x, long long n) {
-//         if(n==0 || x==1)
-//             return 1;
-//         if(x==0)
-//             return 0;
-//         if(n<0)
-//             return myPow(1/x,abs(n));
-
-//         return (n%2==0 ? myPow(x*x,n/2) : x*myPow(x*x,n/2));
-//     }
-// };
-
+// BruteForce Solution
 // class Solution {
 // public:
 //     double myPow(double x, int n) {
-//         double res=1;
-//         if (n == 0) return 1;
-//         if (x == 1) return x;
-//          if(n<0){
-//             x=1/x;
-//             n= abs(n);
-//         }
-//         while(n>0){
-//             if((n & 1)==1){
-//                 res=res*x;
-//             }
-//             x*=x;
-//             n=n>>1;
-//         }
-//         return res;
+//         return pow(x, n);
 //     }
 // };
+// TC: O(log n)
+// SC: O(1)
+
+// Optimized Solution
+// class Solution {
+//     double powPos(double x, long long p) {  // p >= 0
+//         if (p == 0) return 1.0;
+//         double half = powPos(x * x, p / 2);
+//         return (p & 1) ? x * half : half;
+//     }
+// public:
+//     double myPow(double x, int n) {
+//         long long p = n;
+//         if (p < 0) return 1.0 / powPos(x, -p);
+//         return powPos(x, p);
+//     }
+// };
+// TC: O(log n)
+// SC: O(log n)
+
+// Optimized Solution 2.0
+class Solution {
+public:
+    double myPow(double x, int n) {
+        long long p = n;
+        if (p < 0) x = 1.0 / x, p = -p;
+        double res = 1.0;
+        while (p) {
+            if (p & 1) res *= x; // if it is even x * half, here x comes first
+            x *= x; // Same step x*x for even or odd, this is HALF
+            p >>= 1; // P/2
+        }
+        return res;
+    }
+};
+// TC: O(log n)
+// SC: O(1)
