e166a88f1034a799979b5cf3c68d5d45d5790323
Mahesh Avvaru <maheshavvaru2004@gmail.com>
2026-07-02 23:41:13 +0530

    Time: 202 ms (38.03%), Space: 12.8 MB (38.38%) - LeetHub

--- 0204-count-primes/0204-count-primes.cpp
+++ 0204-count-primes/0204-count-primes.cpp
@@ -1,18 +1,54 @@
 class Solution {
 public:
     int countPrimes(int n) {
-        if (n < 2) return 0; 
-        
-        vector<int> primes(n, 1); 
-        primes[0] = 0;
-        primes[1] = 0;
-        
+        vector<bool> primes(n, true);
+        if (n > 0) primes[0] = false;
+        if (n > 1) primes[1] = false;
         for (int i = 2; i * i < n; ++i) {
             if (primes[i]) {
-                for (int num = i * i; num < n; num += i) primes[num] = 0;
+                for (int j = i * i; j < n; j += i) primes[j] = false;
             }
         }
-        
-        return accumulate(primes.begin(), primes.end(), 0);
+        return count(primes.begin(), primes.end(), true);
     }
 };
+
+// class Solution {
+// public:
+//     int countPrimes(int n) {
+//         vector<bool> primes(n, true);
+//         if (n > 0) primes[0] = false;
+//         if (n > 1) primes[1] = false;
+//         int cnt = 0;
+//         for (int i = 2; i < n; ++i) {
+//             if (primes[i]) {
+//                 cnt += 1;
+//                 for (int j = i * 2; j < n; j += i) primes[j] = false;
+//             }
+//         }
+//         return cnt;
+//     }
+// };
+
+
+
+// class Solution {
+// private:
+//     bool isPrime(int n) {
+//         if (n <= 1) return false;
+//         if (n <= 3) return true;
+//         if (n % 2 == 0 || n % 3 == 0) return false;
+//         for (int i = 5; 1LL * i * i <= n; i += 6) {
+//             if (n % i == 0 || n % (i + 2) == 0) return false;
+//         }
+//         return true;
+//     }
+// public:
+//     int countPrimes(int n) {
+//         int cnt = 0;
+//         for (int i = 2; i < n; ++i) {
+//             if (isPrime(i)) cnt += 1;
+//         }
+//         return cnt;
+//     }
+// };
