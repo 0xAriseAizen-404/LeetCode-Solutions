090d20bb4fe195ba42ff0f6805d6a7fad32f6414
Mahesh Avvaru <maheshavvaru2004@gmail.com>
2026-07-02 22:44:08 +0530

    Time: 0 ms (100%), Space: 10 MB (38.35%) - LeetHub

--- 0078-subsets/0078-subsets.cpp
+++ 0078-subsets/0078-subsets.cpp
@@ -1,20 +1,70 @@
+// // Optimal Solution
 class Solution {
 public:
     vector<vector<int>> subsets(vector<int>& nums) {
-        int n = nums.size();
-        int noOfSubsets = 1 << n;
         vector<vector<int>> res;
-        for (int num = 0; num < noOfSubsets; ++num) {
-            vector<int> v;
-            for (int i = 0; i < n; ++i) {
-                if (num & (1 << i)) v.push_back(nums[i]);
+        int sz = nums.size();
+        for (int mask=0; mask<(1<<sz); ++mask) {
+            vector<int> subset;
+            for (int i=0; i<sz; ++i) {
+                if (mask & (1 << i)) subset.push_back(nums[i]);
             }
-            res.push_back(v);
+            res.push_back(subset);
         }
-        sort(res.begin(), res.end(), [](const vector<int> &a, const vector<int> &b) {
-            if (a.size() == b.size()) return a < b;
-            return a.size() < b.size();
-        });
         return res;
     }
 };
+// // TC: O(n * 2^n)
+// // SC: O(2^n)
+
+// // Better Solution
+// class Solution {
+// private:
+//     void helper(vector<int> &nums, int n, int ind, vector<int> &curr, vector<vector<int>> &res) {
+//         if (ind == n) {
+//             res.push_back(curr);
+//             return;
+//         }
+//         // take
+//         curr.push_back(nums[ind]);
+//         helper(nums, n, ind + 1, curr, res);
+//         curr.pop_back();
+//         // not take
+//         helper(nums, n, ind + 1, curr, res);
+//     }
+// public:
+//     vector<vector<int>> subsets(vector<int> &nums) {
+//         int n = nums.size();
+//         vector<vector<int>> res;
+//         vector<int> curr;
+//         helper(nums, n, 0, curr, res);
+//         return res;
+//     }
+// };
+// // TC: O(n * 2^n)
+// // SC: O(2^n + n)
+
+// // BruteForce Solution
+// class Solution {
+// public:
+//     vector<vector<int>> subsets(vector<int>& nums) {
+//         int n = nums.size();
+//         vector<vector<int>> res = {{}};
+//         // res.push_back({});
+//         for (int i=0; i<n; ++i) { // 1 loop, 2 loop, 3 loop
+//             int sz = res.size();
+//             for (int j=0; j<sz; ++j) {
+//                 vector<int> newSubset = res[j];
+//                 newSubset.push_back(nums[i]);
+//                 res.push_back(newSubset);
+//             }
+//         }
+//         return res;
+//         // Start: {}
+//         // Add 1: {}, {1}
+//         // Add 2: {}, {1}, {2}, {1, 2}
+//         // Add 3: {}, {1}, {2}, {1, 2}, {3}, {1, 3}, {2, 3}, {1, 2, 3}
+//     }
+// };
+// // TC: O(n * 2^n)
+// // SC: O(2^n)
