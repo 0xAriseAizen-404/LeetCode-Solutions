a27c88e477801b9b5e9ffc5e98400f16c014dd62
Mahesh Avvaru <maheshavvaru2004@gmail.com>
2026-08-16 14:28:15 +0530

    Time: 9 ms (51.04%), Space: 28.6 MB (5.45%) - LeetHub

--- 3276-minimum-number-of-pushes-to-type-word-ii/3276-minimum-number-of-pushes-to-type-word-ii.cpp
+++ 3276-minimum-number-of-pushes-to-type-word-ii/3276-minimum-number-of-pushes-to-type-word-ii.cpp
@@ -1,29 +1,66 @@
 class Solution {
 public:
-    void print(auto& c){
-        for(int x: c) cout<<x<<", "; 
-        cout<<endl;
-    }
-    int minimumPushes(string& word) {
-        int freq[26]={0};
-    //    memset(freq, 0, sizeof(freq));
-        for(char c: word) 
-            freq[c-'a']++;
-        sort(freq, freq+26, greater<int>());
-    //    print(freq);
-        int sz=0, push=1, ans=0;
-        for(; sz<26 && freq[sz]!=0; sz++){
-            if (sz>=8 && sz%8==0) push++;
-            ans+=freq[sz]*push;    
+    int minimumPushes(string word) {
+        vector<int> freq(26, 0);
+        for (char ch: word) ++freq[ch - 'a'];
+        sort(freq.rbegin(), freq.rend());
+        int cost = 0;
+        for (int i = 0; i < 26 && freq[i]; i++) {
+            cost += (freq[i] * (i / 8 + 1));
         }
-        return ans;
+        return cost;
     }
 };
+// TC: O(n) + O(26log26) + O(26) = O(n)
+// SC: O(26) = O(1)
 
-auto init = []()
-{ 
-    ios::sync_with_stdio(0);
-    cin.tie(0);
-    cout.tie(0);
-    return 'c';
-}();
+// class Solution {
+// public:
+//     int minimumPushes(string word) {
+//         unordered_map<char, int> umap;
+//         for (char c: word) umap[c]++;
+//         priority_queue<pair<int, char>> pq;
+//         for (auto &[ch, freq]: umap) pq.push({freq, ch});
+//         int cnt = 0, pushes = 1, cost = 0;
+//         while (!pq.empty()) {
+//             cnt += 1;
+//             if (cnt > 8) {
+//                 cnt = 1;
+//                 pushes += 1;
+//             }
+//             auto [freq, ch] = pq.top(); pq.pop();
+//             cost += (pushes * freq);
+//         }
+//         return cost;
+//     }
+// };
+// TC: O(n) + 26*O(log26) + 26*O(log26) = O(n)
+// SC: O(26) + O(26) = O(1)
+
+
+
+// class Solution {
+// public:
+//     int minimumPushes(string word) {
+//         unordered_map<char, int> ump;
+//         for (char c: word) ump[c]++;
+//         auto cmp = [](const pair<char, int> &a, const pair<char, int> &b) {
+//             return a.second < b.second;
+//         };
+//         priority_queue<pair<char, int>, vector<pair<char, int>>, decltype(cmp)> pq(cmp);
+//         for (auto p: ump) pq.push(p);
+//         int cnt = 0;
+//         int pushes = 1;
+//         int cost = 0;
+//         while (!pq.empty()) {
+//             cnt++;
+//             if (cnt > 8) {
+//                 cnt = 1;
+//                 pushes += 1;
+//             }
+//             auto p = pq.top(); pq.pop();
+//             cost += (pushes * p.second);
+//         }
+//         return cost;
+//     }
+// };
